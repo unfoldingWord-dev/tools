829746b41b1f95d5dd55d9f71dddb8f86d48849e
root <root@sfows02.xsni.net>
2014-06-26 00:36:37 +0000

    Added perms fix

--- general_tools/playground_fix.sh
+++ general_tools/playground_fix.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #
 #  Ensures that the playground page says "Feel free to edit this page."
+#  Ensures that permissions are correct in playground, too.
 
 PAGE=/var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages/playground/playground.txt
 MSG="Feel free to edit this page."
 
+chown -R apache:door43 "${PAGE%/*}"
+
 grep -q "$MSG" "$PAGE" && exit 0
 
 echo "$MSG" >"$PAGE"
+chown apache:door43 "$PAGE"
 
 exit 0
