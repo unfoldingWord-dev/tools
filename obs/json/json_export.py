80e9b7a9210ea8df9c4dcc419eaa8dad18018ae6
root <root@sfows02.xsni.net>
2014-07-29 16:39:09 +0000

    Updated license for OBS exported files

--- obs/json/json_export.py
+++ obs/json/json_export.py
@@ -35,7 +35,7 @@ unfoldingWord | Open Bible Stories
 
 *an unrestricted visual mini-Bible in any language*
 
-http://unfoldingword.org/
+http://openbiblestories.com
 
 Created by Distant Shores Media (http://distantshores.org) and the Door43 world missions community (http://door43.org).
 
@@ -43,18 +43,17 @@ Created by Distant Shores Media (http://distantshores.org) and the Door43 world 
 License
 =======
 
-This work is made available under a Creative Commons Attribution-ShareAlike 3.0 Unported License (http://creativecommons.org/licenses/by-sa/3.0).
+This work is made available under a Creative Commons Attribution-ShareAlike 4.0 International License (http://creativecommons.org/licenses/by-sa/4.0/).
 
 You are free:
 
-* to Share— to copy, distribute and transmit the work
-* to Remix — to adapt the work
-* to make commercial use of the work
+* Share — copy and redistribute the material in any medium or format
+* Adapt — remix, transform, and build upon the material for any purpose, even commercially.
 
 Under the following conditions:
 
-* Attribution — You must attribute the work as follows: "Original work available at www.openbiblestories.com." Attribution statements in derivative works should not in any way suggest that we endorse you or your use of this work.
-* ShareAlike — If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
+* Attribution — You must attribute the work as follows: "Original work available at http://openbiblestories.com." Attribution statements in derivative works should not in any way suggest that we endorse you or your use of this work.
+* ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
 
 Use of trademarks: unfoldingWord is a trademark of Distant Shores Media and may not be included on any derivative works created from this content.  Unaltered content from http://openbiblestories.com must include the unfoldingWord logo when distributed to others. But if you alter the content in any way, you must remove the unfoldingWord logo before distributing your work.
 
