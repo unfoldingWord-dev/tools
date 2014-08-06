82626c425496f07db21570896942f0388c29cb89
root <root@sfows02.xsni.net>
2014-08-06 20:39:59 +0000

    added comment about adding notes

--- obs/dokuwiki/create-obs-zip.sh
+++ obs/dokuwiki/create-obs-zip.sh
@@ -22,3 +22,6 @@ find obs -maxdepth 1 -type f -name '[0-9][0-9].txt' | zip $ZIPNAME -@
 zip -r $ZIPNAME obs/front-matter.txt obs/back-matter.txt obs/app_words.txt
 
 echo "Zip at: $LANGOBS/$ZIPNAME.zip"
+
+# To add the notes afterward, you could run this:
+# find obs/notes -type f -name '*.txt' | zip -r $ZIPNAME -@
