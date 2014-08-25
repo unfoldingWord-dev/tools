05ea927389249f753ae7e603f24e3448692c8d16
Jesse A. Griffin <jesse@tummy.com>
2014-08-25 13:26:53 -0600

    Moved fade transition

--- obs/js/reveal_export.py
+++ obs/js/reveal_export.py
@@ -87,7 +87,7 @@ foot = u'''
                 center: true,
 
                 theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
-                transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
+                transition: Reveal.getQueryHash().transition || 'fade', // default/cube/page/concave/zoom/linear/fade/none
 
                 // Parallax scrolling
                 // parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
