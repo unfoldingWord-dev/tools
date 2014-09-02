93539647b4d1cd092e68f9c724d6be17f859ab04
Jesse A. Griffin <jesse@tummy.com>
2014-09-02 10:27:23 -0600

    Added div for meny-arrow

--- obs/js/reveal_export.py
+++ obs/js/reveal_export.py
@@ -28,7 +28,8 @@ except:
 obs_web = '/var/www/vhosts/unfoldingword.org/httpdocs/'
 unfoldingWorddir = '/var/www/vhosts/api.unfoldingword.org/httpdocs/obs/txt/1/'
 uw_img_api = 'http://api.unfoldingword.org/obs/jpg/1/'
-title = u'''    <div class="reveal">
+title = u'''    <div class="meny-arrow"></div>
+    <div class="reveal">
         <div class="slides">
             <section><h1>{0}</h1><h3>{1}</h3></section>'''
 frame = u'<section data-background="{0}"><p>{1}</p></section>'
