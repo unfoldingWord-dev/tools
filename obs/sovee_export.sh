5b019d34434b076b396aecfade84b847ca844cff
Jesse A. Griffin <jesse@tummy.com>
2014-09-04 17:29:10 -0600

    Added script to handle sovee export better

---
+++ obs/sovee_export.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env sh
+# -*- coding: utf8 -*-
+#
+#  Copyright (c) 2014 unfoldingWord
+#  http://creativecommons.org/licenses/MIT/
+#  See LICENSE file for details.
+#
+#  Contributors:
+#  Jesse Griffin <jesse@distantshores.org>
+
+PROGNAME="${0##*/}"
+PANDOC="/usr/bin/pandoc"
+DOKU2HTML=/usr/local/bin/doku2html
+PAGES=/var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages
+
+help() {
+    echo
+    echo "Export Translation Notes files to a single text file."
+    echo
+    echo "Usage:"
+    echo "   $PROGNAME -l <LangCode> --obs"
+    echo "   $PROGNAME --help"
+    echo
+    exit 1
+}
+
+if [ $# -lt 1 ]; then
+    help
+fi
+while test -n "$1"; do
+    case "$1" in
+        --help|-h)
+            help
+            ;;
+        --lang|-l)
+            lang="$2"
+            shift
+            ;;
+        --obs)
+            obs="YES"
+            shift
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            help
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$lang" ]; then
+    echo "Error: language to export must be specified."
+    help
+fi
+
+outputfile="/tmp/obs-tn-$lang-`date +%F`.md"
+echo "Exporting to $outputfile"
+rm -f "$outputfile"
+
+export_file () {
+    f=`cat $1 | sed "s/en/$lang/"`
+    echo "filename: $f" >> "$outputfile"
+    cat "$1" | \
+        sed -e "s/en\/obs\/notes/$lang\/obs\/notes/" \
+            -e "s/en:obs:notes/$lang:obs:notes/" \
+        >> "$outputfile"
+}
+
+# Run the exports
+for f in `find $PAGES/en/obs -type f -name '[0-5][0-9].txt'`; do
+    export_file $f
+done
+
+for f in `find $PAGES/en/obs/notes -type f -name '*.txt'`; do
+    export_file $f
+done
