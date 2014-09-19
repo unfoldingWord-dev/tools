f7bdcda37d974e5eea1c1ecb4b6718c2e5ab89c5
root <root@us.door43.org>
2014-09-19 15:18:11 +0000

    Fixed group

--- general_tools/playground_fix.sh
+++ general_tools/playground_fix.sh
@@ -6,11 +6,11 @@
 PAGE=/var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages/playground/playground.txt
 MSG="Feel free to edit this page."
 
-chown -R apache:door43 "${PAGE%/*}"
+chown -R apache:apache "${PAGE%/*}"
 
 grep -q "$MSG" "$PAGE" && exit 0
 
 echo "$MSG" >"$PAGE"
-chown apache:door43 "$PAGE"
+chown apache:apache "$PAGE"
 
 exit 0
