2c331a883a282005e7a12c4dc1a36855b07aaad0
Jesse A. Griffin <jesse@tummy.com>
2014-10-02 16:21:11 -0600

    Added publish script

---
+++ uw/publish.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+# -*- coding: utf8 -*-
+#
+#  Copyright (c) 2014 unfoldingWord
+#  http://creativecommons.org/licenses/MIT/
+#  See LICENSE file for details.
+#
+#  Contributors:
+#  Jesse Griffin <jesse@distantshores.org>
+
+# Still testing, so exit here
+echo 'running' >>/tmp/publish.out
+id >>/tmp/publish.out
+exit 0
+
+# Run export of OBS to JSON
+/var/www/vhosts/door43.org/tools/obs/json/json_export.py \
+    --unfoldingwordexport >>/tmp/$$.json_export
+
+# Find exported languages, exit if none
+LANGS=`grep "Exporting to unfoldingWord" /tmp/$$.json_export | cut -f 5 -d ' '`
+[ -z "$LANGS" ] && exit 0
+
+# Create PDF via TeX for languages exported
+for LANG in $LANGS; do
+    /var/www/vhosts/door43.org/tools/obs/export.py \
+        -l $LANG -f tex /tmp/$$.$LANG.tex
+    #3 Run context on TeX file to get PDF
+done
+
+# Create web reveal.js viewer
+/var/www/vhosts/door43.org/tools/obs/js/reveal_export.py
+
+# Cleanup
+rm -f /tmp/$$.*
