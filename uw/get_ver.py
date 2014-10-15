50a72a9ede53369babca8877452f22cae63b1bf3
root <root@us.door43.org>
2014-10-15 21:44:45 +0000

    Added script to return version of language

---
+++ uw/get_ver.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+#  Copyright (c) 2014 unfoldingWord
+#  http://creativecommons.org/licenses/MIT/
+#  See LICENSE file for details.
+#
+#  Contributors:
+#  Jesse Griffin <jesse@distantshores.org>
+#
+
+'''
+Returns version number of specified language.
+'''
+
+import os
+import sys
+import json
+import urllib2
+
+catalog_url = u'https://api.unfoldingword.org/obs/txt/1/obs-catalog.json'
+
+
+def getURL(url):
+    try:
+        request = urllib2.urlopen(url).read()
+        return request
+    except:
+        print '  => ERROR retrieving {0}\nCheck the URL'.format(url)
+        return
+
+
+if __name__ == '__main__':
+    lang = sys.argv[1]
+    cat = json.loads(getURL(catalog_url))
+    for x in cat:
+        if lang == x['language']:
+            print x['status']['version']
+            sys.exit(0)
+    print 'NOT FOUND'
