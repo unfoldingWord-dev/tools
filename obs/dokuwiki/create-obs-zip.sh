f330f7da49240b51d72d75c8a5c022045ed67639
root <root@us.door43.org>
2014-10-27 22:36:11 +0000

    Added a few more files to the zip files

--- obs/dokuwiki/create-obs-zip.sh
+++ obs/dokuwiki/create-obs-zip.sh
@@ -12,16 +12,19 @@ LANG="$1"
 
 [ -z "$LANG" ] && echo "Please specify language code." && exit 1
 
-LANGOBS="/var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages/$LANG"
+PAGES="/var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages"
+LANGDIR="$PAGES/$LANG"
 ZIPNAME="$LANG-obs-`date +%F`"
 
-cd $LANGOBS
+cd $LANGDIR
 
 find obs -maxdepth 1 -type f -name '[0-9][0-9].txt' | zip $ZIPNAME -@
 
-zip -r $ZIPNAME obs/front-matter.txt obs/back-matter.txt obs/app_words.txt
+zip -r $ZIPNAME obs/front-matter.txt obs/back-matter.txt obs/app_words.txt \
+    statement-of-faith.txt legal/license.txt translation-guidelines.txt
 
-echo "Zip at: $LANGOBS/$ZIPNAME.zip"
+mv $ZIPNAME.zip /tmp/
+echo "Zip at: /tmp/$ZIPNAME.zip"
 
 # To add the notes afterward, you could run this:
 # find obs/notes -type f -name '*.txt' | zip -r $ZIPNAME -@
