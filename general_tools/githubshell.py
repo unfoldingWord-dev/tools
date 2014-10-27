0728899489f0c54a30c786bd685773205f8a5f68
root <root@us.door43.org>
2014-10-27 19:40:11 +0000

    Added simple wrapper for testing github api

---
+++ general_tools/githubshell.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+#  Copyright (c) 2014 unfoldingWord
+#  http://creativecommons.org/licenses/MIT/
+#  See LICENSE file for details.
+#
+#  Contributors:
+#  Jesse Griffin <jesse@distantshores.org>
+#
+#  Requires PyGithub.
+
+'''
+This provides easy access to the PyGithub API for testing and development.
+'''
+
+import sys
+sys.path.append('/var/www/vhosts/door43.org/tools/general_tools')
+try:
+    from git_wrapper import *
+except:
+    print "Please verify that"
+    print "/var/www/vhosts/door43.org/tools/general_tools exists."
+    sys.exit(1)
+try:
+    from github import Github
+    from github import GithubException
+except:
+    print "Please install PyGithub with pip"
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    # Log in to Github via API
+    try:
+        pw = open('/root/.github_pass', 'r').read().strip()
+        guser = githubLogin('dsm-git', pw)
+        githuborg = getGithubOrg('door43', guser)
+    except GithubException as e:
+        print 'Problem logging into Github: {0}'.format(e)
+        sys.exit(1)
