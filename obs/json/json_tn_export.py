bc29eb94f693ded5fa12dbf0469da31f49ccca10
root <root@us.door43.org>
2014-10-29 20:22:15 +0000

    Fixed bold replacement, removed spaces from titles

--- obs/json/json_tn_export.py
+++ obs/json/json_tn_export.py
@@ -26,7 +26,7 @@ root = '/var/www/vhosts/door43.org/httpdocs/data/gitrepo'
 pages = os.path.join(root, 'pages')
 api = '/var/www/vhosts/api.unfoldingword.org/httpdocs/obs/txt/1/'
 
-# Regexes for splitting the chapter into components
+# Regexes for grabbing content
 ktre = re.compile(ur'====== (.*?) ======', re.UNICODE)
 subre = re.compile(ur'\n==== (.*) ====\n', re.UNICODE)
 defre = re.compile(ur'===== Definition: =====(.*?)\[See also', re.UNICODE | re.DOTALL)
@@ -40,11 +40,15 @@ examplesre = re.compile(ur'===== Examples from the Bible stories.*',
 extxtre = re.compile(ur'\*\* (.*)', re.UNICODE)
 fridre = re.compile(ur'[0-5][0-9]-[0-9][0-9]', re.UNICODE)
 
+# Regexes for DW to HTML conversion
+boldstartre = re.compile(ur'([ ,.])(\*\*)', re.UNICODE)
+boldstopre = re.compile(ur'(\*\*)([ ,.])', re.UNICODE)
+
 
 def getKT(f):
     page = codecs.open(f, 'r', encoding='utf-8').read()
     kt = {}
-    kt['term'] = ktre.search(page).group(1)
+    kt['term'] = ktre.search(page).group(1).strip()
     kt['sub'] = getKTSub(page)
     kt['def_title'], kt['def'] = getKTDef(page)
     kt['cf'] = getKTCF(page)
@@ -92,10 +96,13 @@ def getKTExamples(page):
         ex['text'] = extxtre.search(i).group(1).strip()
         ex['text'] = getHTML(ex['text'])
         examples.append(ex)
+    return examples
 
 def getHTML(text):
     # add ul/li
-    return text.replace(' **', ' <b>').replace('** ', '</b> ')
+    text = boldstartre.sub(ur'\1<b>', text)
+    text = boldstopre.sub(ur'</b>\2', text)
+    return text
 
 def writeJSON(outfile, p):
     f = codecs.open(outfile, 'w', encoding='utf-8')
