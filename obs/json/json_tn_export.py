72c9a5f97e9e3303da98a5c98dd90d564f8f080e
root <root@us.door43.org>
2014-11-04 20:08:26 +0000

    Fixed text finds for tN

--- obs/json/json_tn_export.py
+++ obs/json/json_tn_export.py
@@ -42,7 +42,7 @@ fridre = re.compile(ur'[0-5][0-9]-[0-9][0-9]', re.UNICODE)
 tNre = re.compile(ur'==== Translation Notes.*', re.UNICODE | re.DOTALL)
 itre = re.compile(ur'==== Important Terms: ====(.*?)====', re.UNICODE | re.DOTALL)
 tNtermre = re.compile(ur' \*\*(.*?)\*\* ', re.UNICODE)
-tNtextre = re.compile(ur'\*\* - (.*)', re.UNICODE)
+tNtextre = re.compile(ur'\*\* [–-] (.*)', re.UNICODE)
 
 # Regexes for DW to HTML conversion
 boldstartre = re.compile(ur'([ ,.])(\*\*)', re.UNICODE)
@@ -155,7 +155,7 @@ def getIT(page):
 def gettN(page):
     tN = []
     text = tNre.search(page).group(0)
-    for i in text.split():
+    for i in text.split('\n'):
         item = {}
         tNtermse = tNtermre.search(i)
         if not tNtermse:
