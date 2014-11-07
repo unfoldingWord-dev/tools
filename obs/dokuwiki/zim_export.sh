a8e00d30cfa72d2ad9615cf004ca032eca41e5dd
Jesse A. Griffin <jesse@tummy.com>
2014-11-06 17:26:15 -0700

    Added images and zipped result

--- obs/dokuwiki/zim_export.sh
+++ obs/dokuwiki/zim_export.sh
@@ -10,6 +10,8 @@
 
 PROGNAME="${0##*/}"
 PAGES=/var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages
+API=https://api.unfoldingword.org/obs/jpg/1
+DEST=/tmp/zim
 
 help() {
     echo
@@ -34,10 +36,6 @@ while test -n "$1"; do
             srclang="$2"
             shift
             ;;
-        --destination|-d)
-            DEST="$2"
-            shift
-            ;;
         *)
             echo "Unknown argument: $1"
             help
@@ -51,15 +49,10 @@ if [ -z "$srclang" ]; then
     help
 fi
 
-# NEEDED?
-depth="..\/..\/..\/"
+ZIMZIP=/tmp/obs-$srclang-zim.zip
 
-
-echo -n "Remove and export to $DEST? [N/y] "
-read CONT
-[ "$CONT" != 'y' ] && exit 1
 rm -rf "$DEST"
-mkdir "$DEST"
+mkdir -p "$DEST"
 
 export_file () {
     mkdir -p `echo "$DEST/${1##$PAGES/}" | grep -o '.*/'`
@@ -72,6 +65,7 @@ export_file () {
         -e 's/^\*\[/\* \[/g' \
         -e 's/\]\]\/\//\]\]/' \
         -e 's/\/\/see/see/' \
+        -e "s/{{https:\/\/api.unfoldingword.org\/obs\/jpg\/1\/$srclang\/360px/{{..\/360px/" \
         "$1" | grep -v -e ":playground:" -e '^~~' \
         > "$DEST/${1##$PAGES/}"
 }
@@ -86,6 +80,12 @@ export_file $PAGES/$srclang/get-started.txt
 export_file $PAGES/$srclang/tips-tricks.txt
 export_file $PAGES/$srclang/obs-training/get-started/switching_editors.txt
 
+if [ ! -f /tmp/obs-images-360px.zip ]; then
+    cd /tmp/
+    wget -q "$API/$srclang/obs-images-360px.zip"
+fi
+unzip -q /tmp/obs-images-360px.zip -d "$DEST/$srclang/obs/"
+
 for f in `find $PAGES/$srclang/obs -maxdepth 1 -type f -name '[0-5][0-9].txt' | sort`; do
     export_file $f
 done
@@ -95,3 +95,8 @@ for f in `find $PAGES/$srclang/obs/notes -type f -name '*.txt' | sort`; do
     [ "$f" == "$PAGES/$srclang/obs/notes/key-terms/1-discussion-topic.txt" ] && continue
     export_file $f
 done
+
+rm -f $ZIMZIP
+cd /tmp/
+zip -q -r $ZIMZIP zim
+echo "Zim zip at $ZIMZIP"
