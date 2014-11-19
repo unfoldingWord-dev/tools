ec518455a8e285d67c1135625e5ad73bac6229f0
root <root@us.door43.org>
2014-11-19 17:30:41 +0000

    Added user agent string to wget

--- obs/dokuwiki/progress_report.sh
+++ obs/dokuwiki/progress_report.sh
@@ -23,7 +23,7 @@ echo '------' >>"$PROGRESS_FILE"
 echo >>"$PROGRESS_FILE"
 
 echo -n "Published languages (" >>"$PROGRESS_FILE"
-wget -q -O - $PUB_URL | grep 'language' >/tmp/published_langs.txt
+wget -U 'd43' -q -O - $PUB_URL | grep 'language' >/tmp/published_langs.txt
 PUB_LANGS=`wc -l /tmp/published_langs.txt | grep -o "[0-9]"`
 echo -n "$PUB_LANGS): " >>"$PROGRESS_FILE"
 cat /tmp/published_langs.txt | cut -f 2 -d ':' | cut -f 2 -d '"' | tr '\n' ' ' >>"$PROGRESS_FILE"
@@ -36,7 +36,7 @@ echo "%" >>"$PROGRESS_FILE"
 
 echo >>"$PROGRESS_FILE"
 echo -n "Active in the last month (" >>"$PROGRESS_FILE"
-wget -q -O - $CHANGES_URL | grep -o '\/[a-zA-Z0-9\-]*\/obs\/' | cut -f 2 -d '/' | sort | uniq >/tmp/progress_langs.txt
+wget -U 'd43' -q -O - $CHANGES_URL | grep -o '\/[a-zA-Z0-9\-]*\/obs\/' | cut -f 2 -d '/' | sort | uniq >/tmp/progress_langs.txt
 PROGRESS_LANGS=`wc -l /tmp/progress_langs.txt | grep -o "[0-9]*"`
 echo -n "$PROGRESS_LANGS): " >>"$PROGRESS_FILE"
 cat /tmp/progress_langs.txt | tr '\n' ' ' >>"$PROGRESS_FILE"
