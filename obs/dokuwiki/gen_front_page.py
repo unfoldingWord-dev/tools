e2fdca943d4c3b2a3e4d1169f01946c2ef4ed96b
root <root@us.door43.org>
2014-11-26 19:30:21 +0000

    Added script to generate door43 main page

---
+++ obs/dokuwiki/gen_front_page.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+#  Copyright (c) 2014 unfoldingWord
+#  http://creativecommons.org/licenses/MIT/
+#  See LICENSE file for details.
+#
+#  Contributors:
+#  Jesse Griffin <jesse@distantshores.org>
+#
+
+'''
+Returns version number of specified language.
+'''
+
+import os
+import sys
+import json
+import codecs
+import urllib2
+
+outfile = '/var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages/d43_home.txt'
+lang_url = u'http://td.unfoldingword.org/exports/langnames.json'
+conf_path = u'/var/www/vhosts/door43.org/httpdocs/conf/local.php'
+entry_tmpl = u'  * [[:{0}:home|{1} ({0})]]'
+page_tmpl = u'''
+~~NOCACHE~~
+
+**Choose your language:**
+
+{0}
+
+----
+
+Looking for a language that is not here yet?[[http://distantshores.org/contact|Contact us]] and we will configure it for use (it only takes a few minutes).
+
+FAQ – Frequently Asked Questions
+'''
+lang_dict = {}
+
+def getURL(url):
+    try:
+        request = urllib2.urlopen(url).read()
+        return request
+    except:
+        print '  => ERROR retrieving {0}\nCheck the URL'.format(url)
+        return
+
+def getConf(path):
+    for line in open(path, 'r').readlines():
+        if 'translations' in line:
+            codes = line.split(';')[0].replace("'", "").split(' ')[2:]
+            break
+    return codes
+
+def genPage(codes, langs):
+    for e in langs:
+        lang_dict[e['lc']] = e['ln']
+    code_list = []
+    for lc in codes:
+        if not lang_dict.has_key(lc):
+            print "Code not found: {0}".format(lc)
+            continue
+        code_list.append(entry_tmpl.format(lc, lang_dict[lc]))
+    page = page_tmpl.format(u'\n'.join(code_list))
+    f = codecs.open(outfile, 'w', encoding='utf-8')
+    f.write(page)
+    f.close()
+
+
+if __name__ == '__main__':
+    langs = json.loads(getURL(lang_url))
+    conf = getConf(conf_path)
+    genPage(conf, langs)
