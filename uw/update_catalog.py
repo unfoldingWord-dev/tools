eaf90fb4eb27b7d3da210c768d7dc7a7fda51de6
Jesse A. Griffin <jesse@tummy.com>
2015-01-13 15:23:14 -0700

    Added initial catalog generator for tS v2 api

---
+++ uw/update_catalog.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+#  Copyright (c) 2015 unfoldingWord
+#  http://creativecommons.org/licenses/MIT/
+#  See LICENSE file for details.
+#
+#  Contributors:
+#  Jesse Griffin <jesse@distantshores.org>
+#
+
+'''
+Updates the catalog for the translationStudio v2 API.
+'''
+
+import os
+import sys
+import json
+import codecs
+import urllib2
+import argparse
+
+obs_v1_api = u'https://api.unfoldingword.org/obs/txt/1'
+obs_v1_url = u'{0}/obs-catalog.json'.format(obs_v1_api)
+obs_v2_api = u'/var/www/vhosts/api.unfoldingword.org/httpdocs/ts/txt/2'
+
+
+def getURL(url):
+    try:
+        request = urllib2.urlopen(url).read()
+        return request
+    except:
+        print '  => ERROR retrieving {0}\nCheck the URL'.format(url)
+        return
+
+def writeFile(outfile, p):
+    makeDir(outfile.rpartition('/')[0])
+    f = codecs.open(outfile, 'w', encoding='utf-8')
+    f.write(p)
+    f.close()
+
+def makeDir(d):
+    if not os.path.exists(d):
+        os.makedirs(d, 0755)
+
+def getDump(j):
+    return json.dumps(j, sort_keys=True, indent=2)
+
+def loadJSON(f, t):
+    if os.path.isfile(f):
+        return json.load(open(f, 'r'))
+    if t == 'd':
+      return json.loads('{}')
+    else:
+      return json.loads('[]')
+
+def obs():
+    obs_v1 = getURL(obs_v1_url)
+    obs_v1_cat = json.loads(obs_v1)
+    obs_v2_cat = []
+    for e in obs_v1_cat:
+        del e['string']
+        del e['direction']
+        e['slug'] = 'obs'
+        e['name'] = 'Open Bible Stories'
+        e['source'] = u'{0}/{1}/obs-{1}.json'.format(obs_v1_api,
+                                                                e['language'])
+        e['terms'] = u'{0}/{1}/kt-{1}.json'.format(obs_v1_api, e['language'])
+        e['notes'] = u'{0}/{1}/tN-{1}.json'.format(obs_v1_api, e['language'])
+        outfile = u'{0}/obs/{1}/resources.json'.format(obs_v2_api,
+                                                                e['language'])
+        writeFile(outfile, getDump([e]))
+
+def main():
+    obs()
+
+
+if __name__ == '__main__':
+    main()
