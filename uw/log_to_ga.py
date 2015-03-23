4bf37863643e9305f620e77595a20f41ef8b2a77
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2015-03-23 07:45:07 -0400

    Process log files

---
+++ uw/log_to_ga.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+#  Copyright (c) 2015 unfoldingWord
+#  http://creativecommons.org/licenses/MIT/
+#  See LICENSE file for details.
+#
+#  Contributors:
+#  Phil Hopper <phillip_hopper@wycliffeassociates.org>
+#
+
+import os
+import glob
+import json
+import gzip
+import re
+
+
+class ProcessApiLog:
+
+    def __init__(self):
+        pass
+
+    # This is the name of the current log file, and the prefix for log file backups
+    logFileRoot = '/var/log/nginx/api.unfoldingword.org.log'
+
+    settingsFile = os.path.join(os.path.dirname(__file__), 'lastSentToGA.json')
+    hostName = 'https://api.unfoldingword.org'
+
+    # initialize last sent date
+    lastSent = '20150322'
+    currentSent = ''
+
+    def do_it(self):
+
+        # Example: api.unfoldingword.org.log-20150314.gz
+        logfiles = glob.glob(self.logFileRoot + '-*.gz')
+        processedcount = 0
+
+        self.get_last_sent()
+
+        for logFile in logfiles:
+
+            filedate = self.get_file_date(logFile)
+
+            if filedate > self.lastSent:
+                processedcount += self.process_this_file(logFile)
+
+            if filedate > self.currentSent:
+                self.currentSent = filedate
+
+        if processedcount == 0:
+            print 'Finished. No log files processed'
+        elif processedcount == 1:
+            print 'Finished. Processed 1 log file.'
+        else:
+            print 'Finished. Processed ' + str(processedcount) + ' log files.'
+
+    def get_last_sent(self):
+
+        if os.path.exists(self.settingsFile):
+
+            data = {}
+
+            try:
+                with open(self.settingsFile) as settings:
+                    data = json.load(settings)
+
+            except ValueError:
+                print self.settingsFile + ' is not a valid json file'
+
+            if data:
+                if 'lastSent' in data:
+                    self.lastSent = data['lastSent']
+
+    @staticmethod
+    def get_file_date(logfile):
+        return logfile[-11:-3]
+
+    @staticmethod
+    def process_this_file(logfile):
+
+        if not os.path.exists(logfile):
+            return 0
+
+        # regex for parsing the log entries
+        # values: [0]ip_address [1]unk [2]remote_user
+        # [3]timestamp [4]request [5]status [6]bytes_sent
+        # [7]referer [8]user_agent [9]forwarded_for
+        regex = '([\d\.]+) (.*?) (.*?) \[(.*?)\] "(.*?)" (.*?) (.*?) "(.*?)" "(.*?)" "(.*?)"'
+
+        # open the archive and read each line
+        with gzip.GzipFile(logfile) as archive:
+            for line in archive:
+
+                # skip lines produced by check_http
+                if '"check_http' in line:
+                    continue
+
+                # skip favicon
+                if 'favicon.ico' in line:
+                    continue
+
+                # split the line into fields
+                fields = re.match(regex, line.rstrip()).groups()
+
+                # skip bots
+                if 'bot' in fields[8]:
+                    continue
+
+                if 'https' not in line:
+                    continue
+
+                # print line.rstrip()
+                requestparts = fields[4].split(' ')
+                request = ProcessApiLog.hostName + requestparts[1]
+                print request
+
+        # increment the count when finished
+        return 1
+
+
+if __name__ == '__main__':
+    lp = ProcessApiLog()
+    lp.do_it()
