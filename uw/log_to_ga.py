54245864074bae5a57531edb29c9ef2f4f555336
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2015-03-29 19:12:48 -0400

    Shutdown code Improve logging Basic exception handling

--- uw/log_to_ga.py
+++ uw/log_to_ga.py
@@ -9,18 +9,25 @@
 #  Phil Hopper <phillip_hopper@wycliffeassociates.org>
 #
 
-import calendar
+import os
 import re
 import httplib
 import logging
 import time
 import subprocess
 import select
-
+import atexit
+import traceback
 
 # enable logging for this script
 logging.basicConfig(filename='event.log', level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
-logging.info('Begin sending api.unfoldingword.org log to Google Analytics.')
+logging.info('Begin tailing api.unfoldingword.org log for Google Analytics.')
+
+
+# stop the script by deleting this pid file
+pidFile = 'log_to_ga.pid'
+with open(pidFile, 'w') as f:
+    f.write(str(os.getpid()))
 
 
 class GoogleConnection(httplib.HTTPConnection):
@@ -39,17 +46,18 @@ class GoogleConnection(httplib.HTTPConnection):
 # This is the name of the current log file, and the prefix for log file backups
 logFile = '/var/log/nginx/api.unfoldingword.org.log'
 hostName = 'api.unfoldingword.org'
-# propertyID = 'UA-60106521-2'  # api.unfoldingword.org
-propertyID = 'UA-37389677-2'  # prayer.hoppenings.net
+propertyID = 'UA-60106521-2'  # api.unfoldingword.org
+
+
+@atexit.register
+def exit_logger():
+    print 'Exiting.'
+    # attempt to log when the script stops, only works if not stopped using kill
+    logging.info('Stopped tailing api.unfoldingword.org log for Google Analytics.')
+    logging.shutdown()
 
 
 def process_this_line(logline):
-
-    # regex for parsing the log entries
-    # values: [0]ip_address [1]unk [2]remote_user
-    # [3]timestamp [4]request [5]status [6]bytes_sent
-    # [7]referer [8]user_agent [9]forwarded_for
-    regex = '([\d\.]+) (.*?) (.*?) \[(.*?)\] "(.*?)" (.*?) (.*?) "(.*?)" "(.*?)" "(.*?)"'
 
     # skip lines produced by check_http
     if '"check_http' in logline:
@@ -59,6 +67,14 @@ def process_this_line(logline):
     if 'favicon.ico' in logline:
         return
 
+    print logline.rstrip()
+
+    # regex for parsing the log entries
+    # values: [0]ip_address [1]unknown [2]remote_user
+    #         [3]timestamp [4]request [5]status [6]bytes_sent
+    #         [7]referer [8]user_agent [9]forwarded_for
+    regex = '([\d\.]+) (.*?) (.*?) \[(.*?)\] "(.*?)" (.*?) (.*?) "(.*?)" "(.*?)" "(.*?)"'
+
     # split the line into fields
     fields = re.match(regex, logline.rstrip()).groups()
 
@@ -66,18 +82,14 @@ def process_this_line(logline):
     if 'bot' in fields[8]:
         return
 
-    # Timestamp example: 21/Mar/2015:04:20:18 +0000
-    timestamp = time.strptime(fields[3], '%d/%b/%Y:%H:%M:%S +0000')
-
     requestparts = fields[4].split(' ')
     # send_hit_to_ga(requestparts[1], timestamp)
 
-    then = calendar.timegm(timestamp)
-    queuetime = (time.time() - then) * 1000
-
     with GoogleConnection('www.google-analytics.com') as connection:
+
         payload = ['v=1', 'tid=' + propertyID, 'cid=555', 't=pageview', 'dh=' + hostName,
-                   'dp=' + requestparts[1], 'qt=' + str(queuetime)]
+                   'dp=' + requestparts[1]]
+
         connection.request('POST', '/collect', '&'.join(payload))
         response = connection.getresponse()
 
@@ -94,13 +106,30 @@ if __name__ == '__main__':
     # This is the name of the current log file
     logFile = '/var/log/nginx/api.unfoldingword.org.log'
 
-    f = subprocess.Popen(['tail', '-F', logFile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # Explanation: -n sets the number of lines to read immediately when starting.
+    #              -F sets the file name to tail, and being upper case it tells tail to
+    #                 retry opening the file if it fails, like when the log file is rotated.
+    f = subprocess.Popen(['tail', '-n', '1', '-F', logFile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     p = select.poll()
     p.register(f.stdout)
 
-    while True:
-        if p.poll(1):
-            line = f.stdout.readline()
-            process_this_line(line)
-            print line
-        time.sleep(1)
+    print 'Tailing ' + logFile + '...'
+
+    running = True
+
+    while running:
+
+        # exit if pid file has been deleted
+        if not os.path.isfile(pidFile):
+            running = False
+            continue
+
+        # noinspection PyBroadException
+        try:
+            if p.poll(1):
+                line = f.stdout.readline()
+                process_this_line(line)
+                time.sleep(1)
+
+        except:
+            logging.error(traceback.format_exc())
