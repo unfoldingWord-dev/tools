594d61a3b917b4b19526156368760b122b9ac507
root <root@us.door43.org>
2015-05-20 23:07:25 +0000

    Updated to cronify

--- general_tools/en_indexer.sh
+++ general_tools/en_indexer.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
 
+### exclude admin-reports
+
 index () {
     cd $1
     for f in `find * -type f | sort -r`; do
-        echo "wget -q -U 'd43' -O /dev/null \
-            http://door43.org/en/$2/${f%%.txt}"
+        #echo "http://door43.org/en/$2/${f%%.txt}"
         wget -q -U 'd43' -O /dev/null http://door43.org/en/$2/${f%%.txt}
     done
 }
 
-index /var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages/en/notes notes
 index /var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages/en/obe obe
 index /var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages/en/ta ta
 index /var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages/en/bible bible
