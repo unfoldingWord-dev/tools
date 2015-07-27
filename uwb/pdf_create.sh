9e65707cf6e1091ed469dd8e637591751effaf54
Richard Mahn <richard_mahn@wycliffeassociates.org>
2015-07-27 15:53:27 -0600

    books are converted to lowercase

--- uwb/pdf_create.sh
+++ uwb/pdf_create.sh
@@ -69,72 +69,72 @@ DATE=`date +"%Y-%m-%d"`
 
 declare -A BOOKS
 BOOKS=(
-    ['GEN']='Genesis'
-    ['EXO']='Exodus'
-    ['LEV']='Leviticus'
-    ['NUM']='Numbers'
-    ['DEU']='Deuteronomy'
-    ['JOS']='Joshua'
-    ['JDG']='Judges'
-    ['RUT']='Ruth'
-    ['1SA']='1 Samuel'
-    ['2SA']='2 Samuel'
-    ['1KI']='1 Kings'
-    ['2KI']='2 Kings'
-    ['1CH']='1 Chronicles'
-    ['2CH']='2 Chronicles'
-    ['EZR']='Ezra'
-    ['NEH']='Nehemiah'
-    ['EST']='Esther'
-    ['JOB']='Job'
-    ['PSA']='Psalms'
-    ['PRO']='Proverbs'
-    ['ECC']='Ecclesiastes'
-    ['SNG']='Song of Solomon'
-    ['ISA']='Isaiah'
-    ['JER']='Jeremiah'
-    ['LAM']='Lamentations'
-    ['EZK']='Ezekiel'
-    ['DAN']='Daniel'
-    ['HOS']='Hosea'
-    ['JOL']='Joel'
-    ['AMO']='Amos'
-    ['OBA']='Obadiah'
-    ['JON']='Jonah'
-    ['MIC']='Micah'
-    ['NAM']='Nahum'
-    ['HAB']='Habakkuk'
-    ['ZEP']='Zephaniah'
-    ['HAG']='Haggai'
-    ['ZEC']='Zechariah'
-    ['MAL']='Malachi'
-    ['MAT']='Matthew'
-    ['MRK']='Mark'
-    ['LUK']='Luke'
-    ['JHN']='John'
-    ['ACT']='Acts'
-    ['ROM']='Romans'
-    ['1CO']='1 Corinthians'
-    ['2CO']='2 Corinthians'
-    ['GAL']='Galatians'
-    ['EPH']='Ephesians'
-    ['PHP']='Philippians'
-    ['COL']='Colossians'
-    ['1TH']='1 Thessalonians'
-    ['2TH']='2 Thessalonians'
-    ['1TI']='1 Timothy'
-    ['2TI']='2 Timothy'
-    ['TIT']='Titus'
-    ['PHM']='Philemon'
-    ['HEB']='Hebrews'
-    ['JAS']='James'
-    ['1PE']='1 Peter'
-    ['2PE']='2 Peter'
-    ['1JN']='1 John'
-    ['2JN']='2 John'
-    ['3JN']='3 John'
-    ['JUD']='Jude'
-    ['REV']='Revelation'
+    ['gen']='Genesis'
+    ['exo']='Exodus'
+    ['lev']='Leviticus'
+    ['num']='Numbers'
+    ['deu']='Deuteronomy'
+    ['jos']='Joshua'
+    ['jdg']='Judges'
+    ['rut']='Ruth'
+    ['1sa']='1 Samuel'
+    ['2sa']='2 Samuel'
+    ['1ki']='1 Kings'
+    ['2ki']='2 Kings'
+    ['1ch']='1 Chronicles'
+    ['2ch']='2 Chronicles'
+    ['ezr']='Ezra'
+    ['neh']='Nehemiah'
+    ['est']='Esther'
+    ['job']='Job'
+    ['psa']='Psalms'
+    ['pro']='Proverbs'
+    ['ecc']='Ecclesiastes'
+    ['sng']='Song of Solomon'
+    ['isa']='Isaiah'
+    ['jer']='Jeremiah'
+    ['lam']='Lamentations'
+    ['ezk']='Ezekiel'
+    ['dan']='Daniel'
+    ['hos']='Hosea'
+    ['jol']='Joel'
+    ['amo']='Amos'
+    ['oba']='Obadiah'
+    ['jon']='Jonah'
+    ['mic']='Micah'
+    ['nam']='Nahum'
+    ['hab']='Habakkuk'
+    ['zep']='Zephaniah'
+    ['hag']='Haggai'
+    ['zec']='Zechariah'
+    ['mal']='Malachi'
+    ['mat']='Matthew'
+    ['mrk']='Mark'
+    ['luk']='Luke'
+    ['jhn']='John'
+    ['act']='Acts'
+    ['rom']='Romans'
+    ['1co']='1 Corinthians'
+    ['2co']='2 Corinthians'
+    ['gal']='Galatians'
+    ['eph']='Ephesians'
+    ['php']='Philippians'
+    ['col']='Colossians'
+    ['1th']='1 Thessalonians'
+    ['2th']='2 Thessalonians'
+    ['1ti']='1 Timothy'
+    ['2ti']='2 Timothy'
+    ['tit']='Titus'
+    ['phm']='Philemon'
+    ['heb']='Hebrews'
+    ['jas']='James'
+    ['1pe']='1 Peter'
+    ['2pe']='2 Peter'
+    ['1jn']='1 John'
+    ['2jn']='2 John'
+    ['3jn']='3 John'
+    ['jud']='Jude'
+    ['rev']='Revelation'
 )
 
 book_export () {
@@ -154,7 +154,7 @@ book_export () {
     HTML_FILE="$BOOK_all.html" # Compilation of all above HTML files
     TMP_FILE="$BOOK_temp.html" # temp stuff
     LINKS_FILE="$BOOK_links.sed" # SED commands for links
-    PDF_FILE="$OUTPUT_DIR/tN_$BOOK_$DATE.pdf"
+    PDF_FILE="$OUTPUT_DIR/tN_${BOOK^^}_$DATE.pdf"
 
     if ! $USE_EXISTING_FILES;
     then
@@ -377,8 +377,7 @@ book_export () {
 #first check all books given are valid
 for arg in "$@"
 do
-    arg=${arg^^} # gives uppercase
-    if [ ! ${BOOKS[$arg]+_} ];
+    if [ ! ${BOOKS[${arg,,}]+_} ];
     then
         echo "Invalid book given: $arg"
         exit 1;
@@ -387,7 +386,6 @@ done
 
 for arg in "$@"
 do
-    arg=${arg^^}
-    book_export $arg
+    book_export ${arg,,}
 done
 
