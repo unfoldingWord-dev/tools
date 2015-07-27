3a1b3f86a4992d182ab3e8c45a78747ba91915d6
Richard Mahn <richard_mahn@wycliffeassociates.org>
2015-07-27 15:26:17 -0600

    Fixed file name, includes list of Bible book names

--- uwb/pdf_create.sh
+++ uwb/pdf_create.sh
@@ -65,16 +65,96 @@ fi
 # Change to own own temp dir but note our current dir so we can get back to it
 pushd $WORKING_DIR
 
+DATE=`date +"%Y-%m-%d"`
+
+declare -A BOOKS
+BOOKS=(
+    ['GEN']='Genesis'
+    ['EXO']='Exodus'
+    ['LEV']='Leviticus'
+    ['NUM']='Numbers'
+    ['DEU']='Deuteronomy'
+    ['JOS']='Joshua'
+    ['JDG']='Judges'
+    ['RUT']='Ruth'
+    ['1SA']='1 Samuel'
+    ['2SA']='2 Samuel'
+    ['1KI']='1 Kings'
+    ['2KI']='2 Kings'
+    ['1CH']='1 Chronicles'
+    ['2CH']='2 Chronicles'
+    ['EZR']='Ezra'
+    ['NEH']='Nehemiah'
+    ['EST']='Esther'
+    ['JOB']='Job'
+    ['PSA']='Psalms'
+    ['PRO']='Proverbs'
+    ['ECC']='Ecclesiastes'
+    ['SNG']='Song of Solomon'
+    ['ISA']='Isaiah'
+    ['JER']='Jeremiah'
+    ['LAM']='Lamentations'
+    ['EZK']='Ezekiel'
+    ['DAN']='Daniel'
+    ['HOS']='Hosea'
+    ['JOL']='Joel'
+    ['AMO']='Amos'
+    ['OBA']='Obadiah'
+    ['JON']='Jonah'
+    ['MIC']='Micah'
+    ['NAM']='Nahum'
+    ['HAB']='Habakkuk'
+    ['ZEP']='Zephaniah'
+    ['HAG']='Haggai'
+    ['ZEC']='Zechariah'
+    ['MAL']='Malachi'
+    ['MAT']='Matthew'
+    ['MRK']='Mark'
+    ['LUK']='Luke'
+    ['JHN']='John'
+    ['ACT']='Acts'
+    ['ROM']='Romans'
+    ['1CO']='1 Corinthians'
+    ['2CO']='2 Corinthians'
+    ['GAL']='Galatians'
+    ['EPH']='Ephesians'
+    ['PHP']='Philippians'
+    ['COL']='Colossians'
+    ['1TH']='1 Thessalonians'
+    ['2TH']='2 Thessalonians'
+    ['1TI']='1 Timothy'
+    ['2TI']='2 Timothy'
+    ['TIT']='Titus'
+    ['PHM']='Philemon'
+    ['HEB']='Hebrews'
+    ['JAS']='James'
+    ['1PE']='1 Peter'
+    ['2PE']='2 Peter'
+    ['1JN']='1 John'
+    ['2JN']='2 John'
+    ['3JN']='3 John'
+    ['JUD']='Jude'
+    ['REV']='Revelation'
+)
+
 book_export () {
-    CL_FILE="$1_cl.html" # Copyrights & Licensing
-    TN_FILE="$1_tn.html" # translationNotes
-    TQ_FILE="$1_tq.html" # translationQuestions
-    TW_FILE="$1_tw.html" # translationWords
-    TA_FILE="$1_ta.html" # translationAcademy
-    HTML_FILE="$1_all.html" # Compilation of all above HTML files
-    TMP_FILE="$1_temp.html" # temp stuff
-    LINKS_FILE="$1_links.sed" # SED commands for links
-    PDF_FILE="$OUTPUT_DIR/uwb_$1.pdf"
+    BOOK=$1
+
+    if [ ! ${BOOKS[$BOOK]+_} ];
+    then
+        echo "Invalid book given: $BOOK"
+        exit 1;
+    fi
+
+    CL_FILE="$BOOK_cl.html" # Copyrights & Licensing
+    TN_FILE="$BOOK_tn.html" # translationNotes
+    TQ_FILE="$BOOK_tq.html" # translationQuestions
+    TW_FILE="$BOOK_tw.html" # translationWords
+    TA_FILE="$BOOK_ta.html" # translationAcademy
+    HTML_FILE="$BOOK_all.html" # Compilation of all above HTML files
+    TMP_FILE="$BOOK_temp.html" # temp stuff
+    LINKS_FILE="$BOOK_links.sed" # SED commands for links
+    PDF_FILE="$OUTPUT_DIR/tN_$BOOK_$DATE.pdf"
 
     if ! $USE_EXISTING_FILES;
     then
@@ -106,11 +186,11 @@ book_export () {
 
         touch $TMP_FILE
 
-        find "$UW_NOTES_DIR/$1" -type f -path "*[0-9]/*[0-9]*.txt" -printf '%P\n' |
+        find "$UW_NOTES_DIR/$BOOK" -type f -path "*[0-9]/*[0-9]*.txt" -printf '%P\n' |
             grep -v 'asv-ulb' |
             sort -u |
             while read f; do
-                wget -U 'me' "$NOTES_URL/$1/${f%%.txt}" -O - |
+                wget -U 'me' "$NOTES_URL/$BOOK/${f%%.txt}" -O - |
                     grep -v '<strong>.*&gt;&gt;<\/a><\/strong>' |
                     grep -v ' href="\/tag\/' \
                     >> $TMP_FILE
@@ -148,10 +228,10 @@ book_export () {
 
         touch $TQ_FILE
 
-        find "$UW_NOTES_DIR/$1" -type f -path "*questions/*[0-9].txt" -printf '%P\n' |
+        find "$UW_NOTES_DIR/$BOOK" -type f -path "*questions/*[0-9].txt" -printf '%P\n' |
             sort |
             while read f; do
-                wget -U 'me' "$NOTES_URL/$1/${f%%.txt}" -O - |
+                wget -U 'me' "$NOTES_URL/$BOOK/${f%%.txt}" -O - |
                     grep -v '<strong>.*&gt;&gt;<\/a><\/strong>' |
                     grep -v ' href="\/tag\/' \
                     >> $TQ_FILE
@@ -174,7 +254,7 @@ book_export () {
         touch $TW_FILE
 
         # Get the linked key terms
-        for term in $(grep -oP '"\/en\/obe.*?"' $TN_FILE | tr -d '"' | sort -u ); do
+        for term in $(grep -oPh '"\/en\/obe.*?"' $TN_FILE | tr -d '"' | sort -u ); do
             wget -U 'me' ${BASE_URL}${term} -O - |
                 grep -v ' href="\/tag\/' \
                 > $TMP_FILE
@@ -273,9 +353,8 @@ book_export () {
         -e 's/"\/_media/"https:\/\/door43.org\/_media/g' \
         $HTML_FILE
 
-    TITLE=$(grep -m 1 'Chapter 01 Comp' $HTML_FILE | cut -f 5 -d '>' | cut -d 'C' -f 1)
-    SUBTITLE="Notes and Text"
-    DATE=`date +"%m/%d/%Y"`
+    TITLE=${BOOKS[$BOOK]}
+    SUBTITLE="translationNotes"
 
     # Create PDF
     pandoc --template=$TEMPLATE -S --toc --toc-depth=2 -V toc-depth=1 \
@@ -294,8 +373,21 @@ book_export () {
     echo "See $PDF_FILE (generated files: $WORKING_DIR)"
 }
 
-for var in "$@"
+
+#first check all books given are valid
+for arg in "$@"
 do
-    book_export $var
+    arg=${arg^^} # gives uppercase
+    if [ ! ${BOOKS[$arg]+_} ];
+    then
+        echo "Invalid book given: $arg"
+        exit 1;
+    fi
+done
+
+for arg in "$@"
+do
+    arg=${arg^^}
+    book_export $arg
 done
 
