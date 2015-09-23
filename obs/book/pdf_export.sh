d37062721a6dc8431bb8d516afd0bea27745c782
Caleb Maclennan <caleb@alerque.com>
2015-09-23 12:49:42 +0300

    Add flag to edit the raw TeX before typesetting
    Sometimes while debugging the typesetting process it's helpful to be able
    to ① see and review the final source code after templating is complete
    and ② have the option to hand edit it to experiment with the result.
    This adds an edit flag to the output script that stops to open the file
    in the system default editor before proceeding.

--- obs/book/pdf_export.sh
+++ obs/book/pdf_export.sh
@@ -19,6 +19,7 @@ help() {
     echo "Options:"
     echo "    -c       Override checking level (1, 2 or 3)"
     echo "    -d       Show debug messages while running script"
+    echo "    -e       Stop to edit the TeX file with \$EDITOR before proceding to typeset"
     echo "    -l LANG  Add language(s) to process"
     echo "    -m MAX   Set a maximum number of chapters to be typeset"
     echo "    -o DIR   Add output location(s) for final PDF"
@@ -31,10 +32,11 @@ help() {
 }
 
 # Process command line options
-while getopts c:dl:m:o:r:t:v:h opt; do
+while getopts c:del:m:o:r:t:v:h opt; do
     case $opt in
         c) checking=$OPTARG;;
         d) debug=true;;
+        e) edit=true;;
         l) langs=("${langs[@]}" "$OPTARG");;
         m) max_chapters=$OPTARG;;
         o) outputs=("${outputs[@]}" "$OPTARG");;
@@ -49,6 +51,7 @@ done
 # Setup variable defaults in case flags were not set
 : ${checking=}
 : ${debug=false}
+: ${edit=false}
 : ${langs[0]=${LANG%_*}}
 : ${max_chapters=0}
 : ${outputs[0]=$(pwd)}
@@ -100,6 +103,9 @@ for lang in "${langs[@]}"; do
 
     # Run python (export.py) to generate the .tex file from template .tex files
     ./obs/export.py -l $lang -m $max_chapters -f tex ${checking:+-c $checking} -o "$BASENAME.tex"
+
+    # If requested, stop to manually edit the finished ConTeXt file before typesetting
+    $edit && $EDITOR "$BASENAME.tex"
 
     # Run ConTeXt (context) to generate stories from .tex file output by python
     $debug && trackers="afm.loading,fonts.missing,fonts.warnings,fonts.names,fonts.specifications,fonts.scaling,system.dump"
