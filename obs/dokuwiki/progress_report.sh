620374d143d6e6e73d1fd72201571c64afe39126
Jesse Griffin <jesse@unfoldingword.org>
2016-01-20 11:57:10 -0700

    Changed format of email slightly

--- obs/dokuwiki/progress_report.sh
+++ obs/dokuwiki/progress_report.sh
@@ -14,13 +14,11 @@ PROGRESS_FILE='/tmp/obs_progress_report.txt'
 LANGS_FILE='/tmp/progress_langs.txt'
 TABLE="/tmp/OBS-Progress-`date +%F`.csv"
 
-echo 'Open Bible Stories' >"$PROGRESS_FILE"
-echo '==================' >>"$PROGRESS_FILE"
+echo 'Open Bible Stories Progress Report' >"$PROGRESS_FILE"
 echo >>"$PROGRESS_FILE"
 date +%F >>"$PROGRESS_FILE"
 echo >>"$PROGRESS_FILE"
-echo 'Online' >>"$PROGRESS_FILE"
-echo '------' >>"$PROGRESS_FILE"
+echo 'Published Online' >>"$PROGRESS_FILE"
 echo >>"$PROGRESS_FILE"
 
 echo -n "Published languages (" >>"$PROGRESS_FILE"
@@ -29,12 +27,16 @@ PUB_LANGS=`cat /tmp/published_langs.txt | tr '}' '\n' | grep -c language`
 echo -n "$PUB_LANGS): " >>"$PROGRESS_FILE"
 cat /tmp/published_langs.txt | tr '}' '\n' | grep -o "language.*" | cut -f 2 -d ' ' | tr '\n' ' ' | tr '\n' ' ' | tr -d '"' | tr -d ',' >>"$PROGRESS_FILE"
 
+echo "You may see these at https://unfoldingword.org/stories/." >>"$PROGRESS_FILE"
+
 echo >>"$PROGRESS_FILE"
 echo >>"$PROGRESS_FILE"
 echo -n "Percentange of all languages: " >>"$PROGRESS_FILE"
 echo "scale=2; $PUB_LANGS * 100 / 7106" | bc | tr -d '\n' >>"$PROGRESS_FILE"
 echo "%" >>"$PROGRESS_FILE"
+echo >>"$PROGRESS_FILE"
 
+echo 'Activity on Door43' >>"$PROGRESS_FILE"
 echo >>"$PROGRESS_FILE"
 echo -n "Active in the last month (" >>"$PROGRESS_FILE"
 wget -U 'd43' -q -O - $CHANGES_URL | grep -o '\/[a-zA-Z0-9\-]*\/obs\/[0-5][0-9]' >/tmp/changes.txt
