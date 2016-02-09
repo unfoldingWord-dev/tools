bb208cd100ba877995a81e3d904c7b0203fb6944
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-02-09 15:09:15 -0700

    fixed line endings

--- ta/html_export.py
+++ ta/html_export.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 #
-#  Copyright (c) 2014 unfoldingWord
+#  Copyright (c) 2016 unfoldingWord
 #  http://creativecommons.org/licenses/MIT/
 #  See LICENSE file for details.
 #
