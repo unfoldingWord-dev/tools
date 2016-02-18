f6c9b11f3ae3553bcce7726132e341f550283c19
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-02-18 15:51:52 -0700

    Now removes translator email and phone

--- gogs/push_repo.py
+++ gogs/push_repo.py
@@ -37,17 +37,52 @@ def push(repo_path, username = None):
         result = api.createRepo(repo)
         if result != api.STATUS_ERROR_CREATING_REPO:
             print "Repo result: ",repo_name,' ',result
+
+            os.chdir(repo_path)
+
+            #Removes 'translators' from manifest.json:
+            filename = 'manifest.json'
+            if os.path.exists(filename) and os.stat(filename).st_size > 0:
+                with open(filename) as data:
+                    data = json.load(data)
+                #Totally remove old file from repo
+                os.system('git filter-branch --force --index-filter "git rm --cached --ignore-unmatch {0}" --prune-empty --tag-name-filter cat -- --all'.format(filename))
+                if 'translators' in data:
+                    for translator in data['translators']:
+                        if 'email' in translator:
+                            del translator['email']
+                        if 'phone' in translator:
+                            del translator['phone']
+                json_text = json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+                with open(filename+'2', 'w') as file:
+                    print >> file, json_text
+                os.system('git commit -a -m "Updated {0}"'.format(filename+'2'));
+
+            #Removes 'translators' from project.json:
+            filename = 'project.json'
+            if os.path.exists(filename) and os.stat(filename).st_size > 0:
+                with open(filename) as data:
+                    data = json.load(data)
+                #Totally remove old file from repo
+                os.system('git filter-branch --force --index-filter "git rm --cached --ignore-unmatch {0}" --prune-empty --tag-name-filter cat -- --all'.format(filename))
+                if 'translators' in data:
+                    for translator in data['translators']:
+                        if 'email' in translator:
+                            del translator['email']
+                        if 'phone' in translator:
+                            del translator['phone']
+                json_text = json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+                with open(filename, 'w') as file:
+                    print >> file, json_text
+                os.system('git commit -a -m "Updated {0}"'.format(filename));
+
             command = '''
                 unset GIT_DIR
                 unset GIT_WORK_TREE
                 git remote add {2} {4}://{0}:{1}@{2}/{0}/{3} &&
-                git filter-branch --force --index-filter "git rm --cached --ignore-unmatch manifest.json project.json" --prune-empty --tag-name-filter cat -- --all &&
                 git push --force --all -u {2} &&
-                git push --force --tags -u {2} &&
-                git checkout -f origin/master
-            '''.format(username, urllib2.quote(user.password), config.api_domain, repo_name, config.api_protocol, config.api_port)
-            os.chdir(repo_path)
-            os.system('pwd')
+                git push --force --tags -u {2}
+            '''.format(username, urllib2.quote(user.password), config.api_domain, repo_name, config.api_protocol)
             os.system(command)
 
 if __name__ == '__main__':
