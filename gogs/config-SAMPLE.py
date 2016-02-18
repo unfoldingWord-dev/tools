656a84d4968ac8ddfb1ec40cb494aaed1c1d9295
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-02-18 14:31:02 -0700

    Added protocol to config

--- gogs/config-SAMPLE.py
+++ gogs/config-SAMPLE.py
@@ -1,6 +1,7 @@
 # Change the values below and save as config.py. Do NOT check into tools repository
-gogs_domain = 'git.door43.org'
-api_base_url = 'https://'+gogs_domain+'/api/v1/{0}'
+api_domain = 'git.door43.org'
+api_protocol = 'https'
+api_base_url = api_protocol+'://'+api_domain+'/api/v1/{0}'
 admin_token = '<api_user_token>'
 admin_username = '<api_user_name>'
 admin_password = '<api_user_password>'
