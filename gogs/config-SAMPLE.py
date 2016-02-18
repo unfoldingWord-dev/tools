ac9202a4c876be2039a8ba2008d7d173c6bb7aef
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-02-18 15:44:25 -0700

    Updated to remove transator's phone and email

--- gogs/config-SAMPLE.py
+++ gogs/config-SAMPLE.py
@@ -1,6 +1,7 @@
 # Change the values below and save as config.py. Do NOT check into tools repository
 api_domain = 'git.door43.org'
 api_protocol = 'https'
+api_port = '80'
 api_base_url = api_protocol+'://'+api_domain+'/api/v1/{0}'
 admin_token = '<api_user_token>'
 admin_username = '<api_user_name>'
