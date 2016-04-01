42d6f045af2c82d37ab7bc2f177739079f11aa00
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-04-01 10:38:00 -0600

    Added new scripts for chunking bible into notes

---
+++ uwb/chunk_all_books_into_notes.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env sh
+# -*- coding: utf8 -*-
+#
+#  Copyright (c) 2016 unfoldingWord
+#  http://creativecommons.org/licenses/MIT/
+#  See LICENSE file for details.
+#
+#  Contributors:
+#  Jesse Griffin <jesse@distantshores.org>
+#  Caleb Maclennan <caleb@alerque.com>
+#  Richard Mahn <richard_mahn@wycliffeassociates.org>
+
+NOTES=/var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages/en/bible/notes
+BASEDIR=$(cd $(dirname "$0")/../ && pwd)
+
+book_import () {
+    # $1 is book short name
+    # $2 is number of chapters
+
+    if [ "$1" == "psa" ]; then
+        for x in `seq -f "%03g" 1 $2`; do
+            $BASEDIR/uwb/put_chunks_into_notes.py -l en -b $1 -c $x >/dev/null
+        done
+    else
+        for x in `seq -f "%02g" 1 $2`; do
+            $BASEDIR/uwb/put_chunks_into_notes.py -l en -b $1 -c $x >/dev/null
+        done
+    fi
+
+    cd $NOTES/$1
+    git add .
+    git commit -am "Updated $1 notes"
+    git push origin master
+
+}
+
+book_import luk 24
+book_import rut 4
+book_import tit 3
+book_import 1ti 6
+book_import 2ti 4
+book_import act 28
+book_import gen 50
+
+# These added 2015-01-28
+book_import jon 4
+book_import rom 16
+book_import gal 6
+book_import eph 6
+book_import php 4
+book_import 1th 5
+book_import 2th 3
+book_import phm 1
+book_import heb 13
+book_import jas 5
+book_import 1co 16
+book_import 2co 13
+book_import exo 40
+book_import 1pe 5
+book_import 2pe 3
+book_import 1jn 5
+book_import 2jn 1
+book_import 3jn 1
+book_import jud 1
+book_import rev 22
+book_import col 4
+
+# These added 2015-02-05
+book_import mat 28
+book_import jhn 21
+book_import mrk 16
+book_import lev 27
+book_import num 36
+book_import deu 34
+book_import jos 24
+
+# These added 2015-02-11
+book_import jdg 21
+book_import 1sa 31
+book_import job 42
+book_import 1ki 22
+book_import 2ki 25
+
+# These added 2015-02-17
+book_import 1ch 29
+book_import 2ch 36
+book_import lam 5
+book_import jer 52
+book_import oba 1
+book_import hag 2
+book_import zep 3
+book_import hab 3
+book_import nam 3
+book_import mic 7
+book_import mal 4
+book_import jol 3
+book_import amo 9
+book_import zec 14
+book_import ezr 10
+book_import est 10
+book_import neh 13
+book_import ezk 48
+book_import dan 12
+
+# These added 2015-02-17
+book_import hos 14
+book_import sng 8
+book_import ecc 12
+book_import pro 31
+book_import 2sa 24
+book_import psa 150
+
+# This added 2015-02-23
+book_import isa 66
+
+chown -R apache:apache /var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages/en
---
+++ uwb/put_chunks_into_notes.py
@@ -0,0 +1,330 @@
+#!/usr/bin/env python2
+# -*- coding: utf8 -*-
+#
+#  Copyright (c) 2016 unfoldingWord
+#  http://creativecommons.org/licenses/MIT/
+#  See LICENSE file for details.
+#
+#  Contributors:
+#  Jesse Griffin <jesse@distantshores.org>
+#  Richard Mahn <richard_mahn@wycliffeassociates.org>
+
+'''
+This script takes grabs the ULB text from the local git repo and splits it based on
+custom \s5 markers.  Then it pulls in the UDB and TFT text for the same
+range and ouputs DokuWiki files for the Notes team.  If the notes page
+exists then it only updates the Bible texts and does not alter other content.
+'''
+
+import os
+import re
+import sys
+import glob
+import codecs
+import argparse
+import urllib2
+import unicode_string_utils
+
+CHAPTERFILE = '/var/www/vhosts/door43.org/{0}-{1}/{2}-{3}/{4}.usfm'
+NP = '/var/www/vhosts/door43.org/httpdocs/data/gitrepo/pages/en/bible/notes'
+TFTURL = 'https://door43.org/_export/raw/en/udb/v1/{0}/{1}.usfm'
+hyphenfixre = re.compile(ur'[ ]?--[ ]?')
+refre = re.compile(ur'\\v.([0-9][0-9]?[0-9]?)')
+httpsre = re.compile(ur'https://pad.door43.org.*', re.UNICODE)
+sectionre = re.compile(ur'\\s.*', re.UNICODE)
+tftp = re.compile(ur'(TFT: =====.*?<usfm>).*?(</usfm>)', re.DOTALL | re.UNICODE)
+udbp = re.compile(ur'(UDB: =====.*?<usfm>).*?(</usfm>)', re.DOTALL | re.UNICODE)
+ulbp = re.compile(ur'(ULB: =====.*?<usfm>).*?(</usfm>)', re.DOTALL | re.UNICODE)
+books = { u'GEN': [ u'Genesis' ],
+          u'EXO': [ u'Exodus' ],
+          u'LEV': [ u'Leviticus' ],
+          u'NUM': [ u'Numbers' ],
+          u'DEU': [ u'Deuteronomy' ],
+          u'JOS': [ u'Joshua' ],
+          u'JDG': [ u'Judges' ],
+          u'RUT': [ u'Ruth' ],
+          u'1SA': [ u'1 Samuel' ],
+          u'2SA': [ u'2 Samuel' ],
+          u'1KI': [ u'1 Kings' ],
+          u'2KI': [ u'2 Kings' ],
+          u'1CH': [ u'1 Chronicles' ],
+          u'2CH': [ u'2 Chronicles' ],
+          u'EZR': [ u'Ezra' ],
+          u'NEH': [ u'Nehemiah' ],
+          u'EST': [ u'Esther' ],
+          u'JOB': [ u'Job' ],
+          u'PSA': [ u'Psalms' ],
+          u'PRO': [ u'Proverbs' ],
+          u'ECC': [ u'Ecclesiastes' ],
+          u'SNG': [ u'Song of Solomon' ],
+          u'ISA': [ u'Isaiah' ],
+          u'JER': [ u'Jeremiah' ],
+          u'LAM': [ u'Lamentations' ],
+          u'EZK': [ u'Ezekiel' ],
+          u'DAN': [ u'Daniel' ],
+          u'HOS': [ u'Hosea' ],
+          u'JOL': [ u'Joel' ],
+          u'AMO': [ u'Amos' ],
+          u'OBA': [ u'Obadiah' ],
+          u'JON': [ u'Jonah' ],
+          u'MIC': [ u'Micah' ],
+          u'NAM': [ u'Nahum' ],
+          u'HAB': [ u'Habakkuk' ],
+          u'ZEP': [ u'Zephaniah' ],
+          u'HAG': [ u'Haggai' ],
+          u'ZEC': [ u'Zechariah' ],
+          u'MAL': [ u'Malachi' ],
+          u'MAT': [ u'Matthew' ],
+          u'MRK': [ u'Mark' ],
+          u'LUK': [ u'Luke' ],
+          u'JHN': [ u'John' ],
+          u'ACT': [ u'Acts' ],
+          u'ROM': [ u'Romans' ],
+          u'1CO': [ u'1 Corinthians' ],
+          u'2CO': [ u'2 Corinthians' ],
+          u'GAL': [ u'Galatians' ],
+          u'EPH': [ u'Ephesians' ],
+          u'PHP': [ u'Philippians' ],
+          u'COL': [ u'Colossians' ],
+          u'1TH': [ u'1 Thessalonians' ],
+          u'2TH': [ u'2 Thessalonians' ],
+          u'1TI': [ u'1 Timothy' ],
+          u'2TI': [ u'2 Timothy' ],
+          u'TIT': [ u'Titus' ],
+          u'PHM': [ u'Philemon' ],
+          u'HEB': [ u'Hebrews' ],
+          u'JAS': [ u'James' ],
+          u'1PE': [ u'1 Peter' ],
+          u'2PE': [ u'2 Peter' ],
+          u'1JN': [ u'1 John' ],
+          u'2JN': [ u'2 John' ],
+          u'3JN': [ u'3 John' ],
+          u'JUD': [ u'Jude' ],
+          u'REV': [ u'Revelation' ]
+}
+TMPL = u'''====== {book} {chp}:{refrng} ======
+
+
+===== TFT: =====
+
+<usfm>
+{tft}
+</usfm>
+
+
+===== UDB: =====
+
+<usfm>
+{udb}
+</usfm>
+
+
+===== ULB: =====
+
+<usfm>
+{ulb}
+</usfm>
+
+
+===== Important Terms: =====
+
+  * example
+  * example
+  * example
+  * example
+ 
+
+===== Translation Notes: =====
+
+
+    * **bold words**  - explanation
+    * **bold words**  - explanation
+    * **bold words**  - explanation
+    * **bold words**  - explanation
+    * **bold words**  - explanation
+    * **bold words**  - explanation
+  
+===== Links: =====
+
+  * **[[en/bible/notes:{bk}/questions/comprehension/{chp}|{book} Chapter {chp} Comprehension Questions]]**
+
+ 
+
+**[[en/bible/notes/{bk}/{prvchp}/{prv}|<<]] | [[en/bible/notes/{bk}/{nxtchp}/{nxt}|>>]]**
+
+~~DISCUSSION~~
+
+
+{{{{tag>draft}}}}
+'''
+
+def getText(resource, lang, book, chapter):
+    return fix_text(codecs.open(CHAPTERFILE.format(resource, lang, book, chapter), "r", "utf-8").read())
+
+def splice(ulb, udb, tft, bk, chp):
+    chunks = {}
+    book = books[bk.upper()][0]
+    for i in ulb.split('\n\\s5'):
+        if i.startswith('https') and len(i) < 50: continue
+        ref_list = refre.findall(i)
+        if not ref_list:
+            continue
+        ref = ref_list[0]
+        refrng = '{0}-{1}'.format(ref_list[0], ref_list[-1])
+        if ref_list[0] == ref_list[-1]:
+            refrng = ref_list[0]
+        chunks[ref] = { 'bk': bk,
+                        'chp': chp,
+                        'book': book,
+                        'ref_list': ref_list,
+                        'refrng': refrng,
+                        'filepath': getPath(bk, chp, ref),
+                      }
+        i = httpsre.sub(u'', i)
+        i = sectionre.sub(u'', i)
+        chunks[ref]['ulb'] = i.strip()
+        chunks[ref]['udb'] = getTXT(ref_list, udb)
+        chunks[ref]['tft'] = getTXT(ref_list, tft)
+    return chunks
+
+def getTXT(refs, txt):
+    chunks = []
+    for r in refs:
+        versep = ur'\\v.?{0}[- ].*?\\v'.format(r)
+        versepend = ur'\\v.?{0}[- ].*?$'.format(r)
+        try:
+            verse = re.search(versep, txt, re.DOTALL).group(0)
+        except AttributeError:
+            try:
+                verse = re.search(versepend, txt, re.DOTALL).group(0)
+            except:
+                print 'Warning: reference not found: {0}'.format(r)
+                continue
+        verse = httpsre.sub(u'', verse)
+        verse = sectionre.sub(u'', verse)
+        chunks.append(verse.rstrip('\\v').strip())
+    return '\n'.join(chunks)
+
+def getPath(bk, chp, ref):
+    fill = getFill(bk)
+    return os.path.join(NP, bk, chp.zfill(fill),
+                                            '{0}.txt'.format(ref.zfill(fill)))
+
+def getFill(bk):
+    if 'psa' in bk.lower():
+       return 3
+    return 2
+    
+def writeFile(f, content):
+    makeDir(f.rpartition('/')[0])
+    out = codecs.open(f, encoding='utf-8', mode='w')
+    out.write(content)
+    out.close()
+    os.chown(f, 48, 48)
+
+def makeDir(d):
+    '''
+    Simple wrapper to make a directory if it does not exist.
+    '''
+    if not os.path.exists(d):
+        os.makedirs(d, 0755)
+
+def writeOrAppend(f, chunk):
+    if os.path.exists(f):
+        content, has_non_ascii_chars, errors = unicode_string_utils.open_file_read_unicode(f)
+        content = tftp.sub(ur'\1\n TFTSUB \n\2', content)
+        content = udbp.sub(ur'\1\n UDBSUB \n\2', content)
+        content = ulbp.sub(ur'\1\n ULBSUB \n\2', content)
+        content = ( content.replace(u' TFTSUB ', chunk['tft'])
+                           .replace(u' UDBSUB ', chunk['udb'])
+                           .replace(u' ULBSUB ', chunk['ulb']) )
+    else:
+        content = TMPL.format(**chunk)
+    writeFile(f, content)
+
+def writeChunks(chunked):
+    fill = getFill(bk)
+    refs = [k for k in chunked.iterkeys()]
+    refs.sort(key=int)
+    for k in refs:
+        i = refs.index(k)
+        chunked[k]['prv'] = getNav(refs, i-1, chunked)
+        chunked[k]['nxt'] = getNav(refs, i+1, chunked)
+        chunked[k]['prvchp'] = chp
+        chunked[k]['nxtchp'] = chp
+        if i == 0:
+            chunked[k]['prvchp'] = str(int(chp) - 1).zfill(fill)
+            chunked[k]['prv'] = getLastSection(bk, chunked[k]['prvchp'])
+        if i == (len(refs) - 1):
+            chunked[k]['nxtchp'] = str(int(chp) + 1).zfill(fill)
+            chunked[k]['nxt'] = '1'.zfill(fill)
+        writeOrAppend(chunked[k]['filepath'], chunked[k])
+
+def getLastSection(bk, chp):
+    dirpath = os.path.join(NP, bk, chp)
+    if not os.path.exists(dirpath):
+        return chp
+    seclist = glob.glob('{0}/[0-9]*.txt'.format(dirpath))
+    ref = chp
+    if len(seclist):
+        seclist.sort()
+        ref = seclist[-1].rpartition('/')[-1].rstrip('.txt')
+    return ref
+
+def getNav(refs, i, chunked):
+    fill = getFill(bk)
+    if i == -1:
+        return ''
+    elif i >= len(refs):
+        return '1'.zfill(fill)
+    return chunked[refs[i]]['ref_list'][0].zfill(fill)
+
+def getURL(url):
+    try:
+        request = urllib2.urlopen(url)
+        content = request.read()
+        encoding = request.headers['content-type'].split('charset=')[-1]
+        ucontent = unicode(content, encoding)
+    except:
+        print "  => ERROR retrieving %s\nCheck the URL" % url
+        sys.exit(1)
+    return ucontent
+
+
+def fix_text(source_text):
+    """
+    Removes and replaces specific characters and sequences
+    :param source_text: string
+    :return: string
+    """
+
+    fixed_text = hyphenfixre.sub(u'—', source_text)
+    fixed_text = fixed_text.replace(u' — ', u'—')
+
+    return fixed_text
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-l', '--language', dest="lang", default="en",
+        required=False, help="Book to convert")
+    parser.add_argument('-b', '--book', dest="book",
+        required=True, help="Book to convert")
+    parser.add_argument('-c', '--chapter', dest="chapter",
+        required=True, help="Chapter to convert")
+
+    args = parser.parse_args(sys.argv[1:])
+    main(args.slug.upper(), args.ver, args.book)
+
+
+    lang = args.lang.lower()
+    book = args.book.lower()
+    chapter = args.chapter.lower()
+
+    ulb = getText('ulb', lang, book, chapter)
+    udb = getText('udb', lang, book, chapter)
+    tft = getURL(TFTURL.format(book, chapter.zfill(3)))
+
+    chunked = splice(ulb, udb, tft, bk, chp)
+    writeChunks(chunked)
