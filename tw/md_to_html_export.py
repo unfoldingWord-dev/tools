a66abdee557e1f0c9d220a8577d3ea60793b867b
Richard Mahn <rich.mahn@unfoldingword.org>
2017-07-20 09:47:26 -0600

    Making new ULB/UDB scripts

--- tw/md_to_html_export.py
+++ tw/md_to_html_export.py
@@ -16,7 +16,6 @@
 import os
 import re
 import sys
-import json
 import codecs
 import argparse
 import markdown2
@@ -34,14 +33,14 @@ terms = {}
 
 taUrl = u'https://unfoldingword.org/academy'
 taManualUrls = {
-  'en-ta-intro': u'ta-intro.html',
-  'en-ta-process': u'ta-process.html',
-  'en-ta-translate-vol1': u'ta-translation-1.html',
-  'en-ta-translate-vol2': u'ta-translation-2.html',
-  'en-ta-checking-vol1': u'ta-checking-1.html',
-  'en-ta-checking-vol2': u'ta-checking-2.html',
-  'en-ta-audio': u'ta-audio.html',
-  'en-ta-gl': u'ta-gateway-language.html',
+    'en-ta-intro': u'ta-intro.html',
+    'en-ta-process': u'ta-process.html',
+    'en-ta-translate-vol1': u'ta-translation-1.html',
+    'en-ta-translate-vol2': u'ta-translation-2.html',
+    'en-ta-checking-vol1': u'ta-checking-1.html',
+    'en-ta-checking-vol2': u'ta-checking-2.html',
+    'en-ta-audio': u'ta-audio.html',
+    'en-ta-gl': u'ta-gateway-language.html',
 }
 
 
@@ -62,10 +61,11 @@ def populateWords():
         for f in files:
             term = os.path.splitext(os.path.basename(f))[0]
             content = markdown2.markdown_path(f)
-            content = u'<div id="{0}-{1}" class="word">'.format(category, term)+content+u'</div>'
+            content = u'<div id="{0}-{1}" class="word">'.format(category, term) + content + u'</div>'
             parts = ta_links.split(content)
             if len(parts) == 6 and parts[1] in taManualUrls:
-                content = parts[0]+'"{0}/{1}#{2}_{3}_{4}"'.format(taUrl, taManualUrls[parts[1]], parts[3], parts[2], parts[4])+parts[5]
+                content = parts[0] + '"{0}/{1}#{2}_{3}_{4}"'.format(taUrl, taManualUrls[parts[1]], parts[3], parts[2],
+                                                                    parts[4]) + parts[5]
             content = re.sub(r'href="\.\.\/([^\/"]+)\/([^"]+)\.md"', r'href="#\1-\2"', content)
             soup = BeautifulSoup(content)
             if soup.h1:
@@ -75,7 +75,7 @@ def populateWords():
                 print title
             for i in reversed(range(1, 4)):
                 for h in soup.select('h{0}'.format(i)):
-                    h.name = 'h{0}'.format(i+1)
+                    h.name = 'h{0}'.format(i + 1)
             content = str(soup.div)
             word = TwTerm(term, title, content)
             terms[category][term] = word
@@ -85,7 +85,8 @@ def fix_content(content):
     for category in twOrder:
         for term in terms[category]:
             word = terms[category][term]
-            content = re.sub(ur'#{0}-{1}">{1}<'.format(category, term), ur'#{0}-{1}">{2}<'.format(category, term, word.title),
+            content = re.sub(ur'#{0}-{1}">{1}<'.format(category, term),
+                             ur'#{0}-{1}">{2}<'.format(category, term, word.title),
                              content)
     return content
 
@@ -95,7 +96,7 @@ def main(inpath, outpath, version):
 
     twRoot = inpath
 
-    license = markdown2.markdown_path(twRoot+'/'+'LICENSE.md')
+    license = markdown2.markdown_path(twRoot + '/' + 'LICENSE.md')
     populateWords()
 
     cover = '''<!DOCTYPE html>
@@ -109,7 +110,7 @@ def main(inpath, outpath, version):
   <div style="text-align:center;padding-top:200px" class="break" id="translationWords">
     <img src="https://unfoldingword.org/assets/img/icon-tw.png" width="120">
     <h1 class="h1">translationWords</h1>
-    <h3 class="h3">v'''+version+'''</h3>
+    <h3 class="h3">v''' + version + '''</h3>
   </div>
 </body>
 </html>
@@ -125,10 +126,10 @@ def main(inpath, outpath, version):
 <body>
   <div class="break">
     <span class="h1">Copyrights & Licensing</span>
-'''+license+'''
+''' + license + '''
     <p>
-      <strong>Date:</strong> '''+time.strftime("%Y-%m-%d")+'''<br/>
-      <strong>Version:</strong> '''+version+'''
+      <strong>Date:</strong> ''' + time.strftime("%Y-%m-%d") + '''<br/>
+      <strong>Version:</strong> ''' + version + '''
     </p>
   </div>
 </body>
@@ -157,22 +158,22 @@ def main(inpath, outpath, version):
   </style>
 </head>
 <body>
-'''+content+u'''
+''' + content + u'''
 </body>
 </html>
 '''
 
-    coverFile = outpath+'/cover.html'
+    coverFile = outpath + '/cover.html'
     f = codecs.open(coverFile, 'w', encoding='utf-8')
     f.write(cover)
     f.close()
 
-    licenseFile = outpath+'/license.html'
+    licenseFile = outpath + '/license.html'
     f = codecs.open(licenseFile, 'w', encoding='utf-8')
     f.write(license)
     f.close()
 
-    bodyFile = outpath+'/body.html'
+    bodyFile = outpath + '/body.html'
     f = codecs.open(bodyFile, 'w', encoding='utf-8')
     f.write(body)
     f.close()
@@ -180,13 +181,13 @@ def main(inpath, outpath, version):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-i', '--input', dest="inpath",
-        help="Directory of the tW repo to be compiled into html", required=True)
+                        help="Directory of the tW repo to be compiled into html", required=True)
     parser.add_argument('-o', '--output', dest="outpath", default='.',
-        required=False, help="Output path of the html file")
+                        required=False, help="Output path of the html file")
     parser.add_argument('-v', '--version', dest="version",
-        required=True, help="Version of translationWords")
+                        required=True, help="Version of translationWords")
 
     args = parser.parse_args(sys.argv[1:])
 
