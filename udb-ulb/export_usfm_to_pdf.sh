c6f34eb7d3c7ba0f5c76c089bea38a043ea8dbef
Richard Mahn <rich.mahn@unfoldingword.org>
2017-07-21 05:49:06 -0600

    Retain old naming syntax

--- udb-ulb/export_usfm_to_pdf.sh
+++ udb-ulb/export_usfm_to_pdf.sh
@@ -128,12 +128,12 @@ for BOOK in "${BOOKS[@]}"; do
     then
         BOOK_ARG='-b gen exo lev num deu jos jdg rut 1sa 2sa 1ki 2ki 1ch 2ch ezr neh est job psa pro ecc sng isa jer lam ezk dan hos jol amo oba jon mic nam hab zep hag zec mal'
         SUBTITLE="Old Testament"
-        BASENAME="${RESOURCE^^}_ot"
+        BASENAME="${RESOURCE^^}-OT"
     elif [ $BOOK == 'nt' ];
     then
         BOOK_ARG='-b mat mrk luk jhn act rom 1co 2co gal eph php col 1ti 2ti 1th 2th tit phm heb jas 1pe 2pe 1jn 2jn 3jn jud rev'
         SUBTITLE='New Testament'
-        BASENAME="${RESOURCE^^}_nt"
+        BASENAME="${RESOURCE^^}-NT"
     else
         BOOK_ARG="-b $BOOK"
         NAME=$(tools/catalog/v3/catalog_query.py -l $LANGUAGE -r $RESOURCE -p $BOOK -k title);
@@ -144,7 +144,7 @@ for BOOK in "${BOOKS[@]}"; do
             exit 1
         fi
         SUBTITLE=$NAME
-        BASENAME="$(printf "%02d" ${SORT})_${BOOK^^}"
+        BASENAME="$(printf "%02d" ${SORT})-${BOOK^^}"
         TOC_DEPTH=2
     fi
 
