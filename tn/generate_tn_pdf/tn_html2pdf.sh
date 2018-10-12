3ff3b53585161aa7d25f8cf3a2a494da282bd203
Richard Mahn <rich.mahn@unfoldingword.org>
2018-10-12 11:02:03 -0600

    Update to wk command

--- tn/generate_tn_pdf/tn_html2pdf.sh
+++ tn/generate_tn_pdf/tn_html2pdf.sh
@@ -134,10 +134,11 @@ echo '<!DOCTYPE html>
 
 cp "$MY_DIR/style.css" "./tn_html"
 
-headerfile="file://$OUTPUT_DIR/tn_html/header.html"
-coverfile="file://$OUTPUT_DIR/tn_html/cover.html"
-licensefile="file://$OUTPUT_DIR/tn_html/license.html"
-tafile="file://$OUTPUT_DIR/tn_html/en_tn_65-3JN_v13.html"
+headerfile="$OUTPUT_DIR/tn_html/header.html"
+coverfile="$OUTPUT_DIR/tn_html/cover.html"
+licensefile="$OUTPUT_DIR/tn_html/license.html"
+bodyfile="$OUTPUT_DIR/tn_html/en_tn_65-3JN_v13.html"
 outfile="./tn_pdf/en_tn_65-3JN_v13.pdf"
+
 echo "GENERATING $outfile"
-wkhtmltopdf --encoding utf-8 --outline-depth 3 -O portrait -L 15 -R 15 -T 15 -B 15 --header-html "$headerfile" --header-spacing 2 --footer-center '[page]' cover "$coverfile" cover "$licensefile" toc --disable-dotted-lines --enable-external-links --xsl-style-sheet "$TEMPLATE" "$tafile" "$outfile"
+wkhtmltopdf --javascript-delay 2000 --encoding utf-8 --outline-depth 3 -O portrait -L 15 -R 15 -T 15 -B 15  --header-html "$headerfile" --header-spacing 2 --footer-center '[page]' cover "$coverfile" cover "$licensefile" toc --disable-dotted-lines --enable-external-links --xsl-style-sheet "$TEMPLATE" "$bodyfile" "$outfile"
