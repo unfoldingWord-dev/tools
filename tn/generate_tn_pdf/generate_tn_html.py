3e791442d5c9ecdb7747d5f408dc88b49c387702
Richard Mahn <rich.mahn@unfoldingword.org>
2018-10-16 08:21:10 -0600

    more fixes

--- tn/generate_tn_pdf/generate_tn_html.py
+++ tn/generate_tn_pdf/generate_tn_html.py
@@ -173,7 +173,7 @@ class TnConverter(object):
                     str += ' {0} {1}:{2}'.format(source[3].upper(), source[4], source[5])
                 else:
                     str = '  t{0} {1}'.format(source[1][1].upper(), '/'.join(source[3:]))
-                str += ': BAD RC - {0}'.format(rc)
+                str += ': BAD RC - `{0}`'.format(rc)
                 if self.bad_links[source_rc][rc]:
                     str += ' - change to `{0}`'.format(self.bad_links[source_rc][rc])
                 _print(str)
