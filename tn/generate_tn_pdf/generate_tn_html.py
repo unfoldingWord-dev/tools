e9de966e1929b5be4eb9f96d0ef455cea0934edd
Richard Mahn <rich.mahn@unfoldingword.org>
2018-10-16 01:13:41 -0600

    fixes

--- tn/generate_tn_pdf/generate_tn_html.py
+++ tn/generate_tn_pdf/generate_tn_html.py
@@ -806,25 +806,24 @@ class TnConverter(object):
 
     def get_reference_text(self, rc):
         uses = ''
-        if rc in self.rc_references and len(self.rc_references[rc]):
-            references = []
-            book_title = '{0} '.format(self.book_title)
-            done = {}
-            for reference in self.rc_references[rc]:
-                if '/tn/' in reference and reference not in done:
-                    parts = reference[5:].split('/')
-                    id = 'tn-{0}-{1}-{2}'.format(self.book_id, parts[4], parts[5])
-                    if parts[4] == 'front':
-                        text = 'Intro to {0}'.format(self.book_title)
-                    elif parts[5] == 'intro':
-                        text = '{0} {1} Notes'.format(self.book_title, parts[4].lstrip('0'))
-                    else:
-                        text = '{0}{1}:{2}'.format(book_title, parts[4].lstrip('0'), parts[5].lstrip('0'))
-                        book_title = ''
-                    references.append('<a href="#{0}">{1}</a>'.format(id, text))
-                    done[reference] = True
-            if len(references):
-                uses = '<p>\n(<b>Go back to:</b> {0})\n</p>\n'.format('; '.join(references))
+        references = []
+        book_title = '{0} '.format(self.book_title)
+        done = {}
+        for reference in self.rc_references[rc]:
+            if '/tn/' in reference and reference not in done:
+                parts = reference[5:].split('/')
+                id = 'tn-{0}-{1}-{2}'.format(self.book_id, parts[4], parts[5])
+                if parts[4] == 'front':
+                    text = 'Intro to {0}'.format(self.book_title)
+                elif parts[5] == 'intro':
+                    text = '{0} {1} Notes'.format(self.book_title, parts[4].lstrip('0'))
+                else:
+                    text = '{0}{1}:{2}'.format(book_title, parts[4].lstrip('0'), parts[5].lstrip('0'))
+                    book_title = ''
+                references.append('<a href="#{0}">{1}</a>'.format(id, text))
+                done[reference] = True
+        if len(references):
+            uses = '<p>\n(<b>Go back to:</b> {0})\n</p>\n'.format('; '.join(references))
         return uses
 
     def get_resource_data_from_rc_links(self, text, source_rc):
