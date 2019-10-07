796929c9f617a50baf94df0736a3fbd5f7a181f1
Richard Mahn <richard_mahn@wycliffeassociates.org>
2019-10-07 19:10:56 +0000

    Adds TAG2

--- ta/ta_html2pdf.sh
+++ ta/ta_html2pdf.sh
@@ -38,12 +38,13 @@ mkdir -p "./html"
 mkdir -p "./pdf"
 
 repo="${LANGUAGE}_${RESOURCE}"
-print_url="https://api.door43.org/tx/print?id=${OWNER}/${repo}/${TAG}"
+TAG2="590d872bf5"
+print_url="https://api.door43.org/tx/print?id=${OWNER}/${repo}/${TAG2}"
 archive_url="https://git.door43.org/${OWNER}/${repo}/archive/${TAG}.zip"
 
 cdn_url=$(wget -qO- $print_url)
 echo $cdn_url
-wget "$cdn_url" -O "./html/ta_orig.html"
+wget "$cdn_url" -O "./html/${LANGUAGE}_ta_v${version}_orig.html"
 wget "$archive_url" -O "./${repo}.zip"
 
 unzip -qo "./${repo}.zip"
@@ -67,7 +68,7 @@ echo "Current '$repo' Version is at: ${version}"
 echo "Current '$repo' Publisher is: ${publisher}"
 echo "Current '$repo' Contributors are: ${contributors}"
 
-"$MY_DIR/massage_ta_html.py" -i "./html/ta_orig.html" -o "./html/ta.html" \
+"$MY_DIR/massage_ta_html.py" -i "./html/${LANGUAGE}_ta_v${version}_orig.html" -o "./html/${LANGUAGE}_ta_v${version}.html" \
                              -s "$MY_DIR/style.css" -v $version
 
 echo '<!DOCTYPE html>
@@ -145,7 +146,7 @@ echo '<!DOCTYPE html>
 headerfile="file://$OUTPUT_DIR/html/header.html"
 coverfile="file://$OUTPUT_DIR/html/cover.html"
 licensefile="file://$OUTPUT_DIR/html/license.html"
-tafile="file://$OUTPUT_DIR/html/ta.html"
+tafile="file://$OUTPUT_DIR/html/${LANGUAGE}_ta_v${version}.html"
 outfile="./pdf/${LANGUAGE}_ta_v${version}.pdf"
 echo "GENERATING $outfile"
 wkhtmltopdf --encoding utf-8 --outline-depth 3 -O portrait -L 15 -R 15 -T 15 -B 15 --header-html "$headerfile" --header-spacing 2 --footer-center '[page]' cover "$coverfile" cover "$licensefile" toc --disable-dotted-lines --enable-external-links --xsl-style-sheet "$TEMPLATE" "$tafile" "$outfile"
