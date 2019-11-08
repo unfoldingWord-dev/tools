05ca1ea8adde9cc2ec00165b68b606905a81d660
Richard Mahn <rich.mahn@unfoldingword.org>
2019-11-08 18:37:15 +0800

    ta generator

--- py3/ta/generate_pdf.py
+++ py3/ta/generate_pdf.py
@@ -18,19 +18,16 @@ import re
 import argparse
 import logging
 import tempfile
-import shutil
+import requests
 import subprocess
 import json
 import git
 import markdown2
 import string
-import codecs
-from glob import glob
-from shutil import copy
 from bs4 import BeautifulSoup, Tag
 from html import escape
 from html.parser import HTMLParser
-from weasyprint import HTML
+from weasyprint import HTML, LOGGER
 from ...general_tools.file_utils import load_yaml_object, read_file, write_file
 
 DEFAULT_OWNER = 'unfoldingWord'
@@ -115,6 +112,14 @@ class TaConverter(object):
         self.config = None
         self.file_id = None
 
+    def fix_links(self, html):
+        html = re.sub(r'href="([^"]+/)*([^"/]+)/01.md"', r'href="#\2"', html, flags=re.MULTILINE | re.IGNORECASE)
+        html = re.sub(r'href="(\.\./)+([^"/]+)/*"', r'href="#\2"', html, flags=re.MULTILINE | re.IGNORECASE)
+        html = re.sub(r'href="rc://([^/]+)/tw/dict/(bible/[^"]+)"',
+                      r'href="https://git.door43.org/{0}/\1_tw/src/branch/master/\2.md"'.format(self.owner), html,
+                      flags=re.MULTILINE | re.IGNORECASE)
+        return html
+
     def run(self):
         if not self.working_dir:
             self.working_dir = tempfile.mkdtemp(prefix='ta-')
@@ -124,8 +129,7 @@ class TaConverter(object):
         self.logger.info('WORKING DIR IS {0} FOR {1}'.format(self.working_dir, self.lang_code))
         self.ta_dir = os.path.join(self.working_dir, '{0}_ta'.format(self.lang_code))
         self.html_dir = os.path.join(self.output_dir, 'html')
-        if not os.path.isdir(self.html_dir):
-            os.makedirs(self.html_dir)
+        os.makedirs(self.html_dir, exist_ok=True)
         self.setup_resource_files()
         self.file_id = '{0}_ta_{1}_{2}'.format(self.lang_code, self.ta_tag, self.generation_info['ta']['commit'])
         self.manifest = load_yaml_object(os.path.join(self.ta_dir, 'manifest.yaml'))
@@ -139,25 +143,30 @@ class TaConverter(object):
         html = html_template.safe_substitute(title=self.title)
         self.soup = BeautifulSoup(html, 'html.parser')
         self.soup.html.head.title.string = self.title
-        self.get_cover()
-        self.get_license()
+        # self.get_cover()
+        # self.get_license()
         self.get_toc()
         self.get_articles()
+        self.download_all_images()
         parser = MyHTMLParser()
         parser.feed(str(self.soup))
-        output_html = parser.get_parsed_string()
-        write_file(os.path.join(self.output_dir, '{0}_new.html'.format(self.file_id)), self.soup.prettify())
+        parsed_html = self.fix_links(parser.get_parsed_string())
+        # prettify_html = self.fix_links(self.soup.prettify())
+        write_file(os.path.join(self.output_dir, 'ta.html'.format(self.file_id)), parsed_html)
+        write_file(os.path.join(self.output_dir, '{0}_new.html'.format(self.file_id)), parsed_html)
         write_file(os.path.join(self.output_dir, '{0}_bad_links.json'.format(self.file_id)), self.bad_links)
         write_file(os.path.join(self.output_dir, '{0}_titles.json'.format(self.file_id)), self.titles)
         for bad_link in sorted(self.bad_links.keys()):
             print('{0}: {1}'.format(bad_link, self.bad_links[bad_link]))
-        weasy = HTML(string=output_html, base_url='file://{0}/'.format(self.output_dir))
-        weasy.write_pdf(os.path.join(os.path.join(self.output_dir, '{0}_new.pdf'.format(self.file_id))))
+        LOGGER.setLevel('WARN')
+        weasy = HTML(string=parsed_html, base_url='file://{0}/'.format(self.output_dir))
+        weasy_render = weasy.render()
+        weasy_render.write_pdf(os.path.join(os.path.join(self.output_dir, '{0}_new.pdf'.format(self.file_id))))
 
     def get_cover(self):
         cover_html = '''
-          <article id="cover">
-            <img src="html/logo-uta.png" width="120"/>
+          <article id="main-cover" class="cover">
+            <img src="html/logo-uta.png" alt="UTA"/>
             <h1 id="cover-title">{0}</h1>
             <h2>Version {1}</h2>
           </article>
@@ -171,11 +180,11 @@ class TaConverter(object):
         license_html = '''
           <article id="license">
             <h1 id="license-title">Copyrights & Licensing</h1>
-            <p>
+            <div id="ta-copyright-info">
               <div id="ta-date"><strong>Date:</strong> {0}</div>
               <div id="ta-version"><strong>Version:</strong> {1}</div>
               <div id="ta-published-by"><strong>Published by:</strong> {2}</div>
-            </p>
+            </div>
             {3}
           </article>
         '''.format(self.issued, self.version, self.publisher, license_html)
@@ -196,7 +205,7 @@ class TaConverter(object):
                   <ul id="contents-top-ul">
                 '''.format(toc['title'])
             self.titles['{0}-cover'.format(project['identifier'])] = project['title']
-            toc_html += '<li><a href="#{0}-manual-cover-title">{1}</a>'.format(project['identifier'], project['title'])
+            toc_html += '<li><a href="#{0}-manual-cover-title"><span>{1}</span></a>'.format(project['identifier'], project['title'])
             toc_html += self.get_toc_html(toc)
             toc_html += '</li>'
         toc_html += '</ul></article>'
@@ -213,7 +222,7 @@ class TaConverter(object):
                 self.titles[section['link']] = title
             self.section_count += 1
             link = 'section-container-{0}'.format(self.section_count)
-            toc_html += '<li><a href="#{0}">{1}</a>{2}</li>'.format(link, title, self.get_toc_html(section))
+            toc_html += '<li><a href="#{0}"><span>{1}</span></a>{2}</li>'.format(link, title, self.get_toc_html(section))
         toc_html += '</ul>'
         return toc_html
 
@@ -226,7 +235,8 @@ class TaConverter(object):
             toc = load_yaml_object(os.path.join(project_path, 'toc.yaml'))
             self.config = load_yaml_object(os.path.join(project_path, 'config.yaml'))
             articles_html += '''
-                <article id="{0}-manual-cover">
+                <article id="{0}-manual-cover" class="manual-cover cover">
+                  <img src="html/logo-uta.png" alt="UTA" />
                   <h1>{1}</h1>
                   <h2 id="{0}-manual-cover-title">{2}</h2>
                 </article>
@@ -242,11 +252,13 @@ class TaConverter(object):
             self.section_count += 1
             link = 'section-container-{0}'.format(self.section_count)
             title = section['title']
-            articles_html += '<section id="{0}"><h{1} id="{0}-title">{2}</h{1}>'.\
-                format(link, level, self.get_title(project, link, title))
+            articles_html += '<section id="{0}">'.format(link)
+            section_header = '<h{0} id="{1}-title" class="section-header">{2}</h{0}>'.format(level, link, self.get_title(project, link, title))
             if 'link' in section:
                 link = section['link']
-                articles_html += self.get_article(project, link)
+                articles_html += self.get_article(project, link, section_header)
+            else:
+                articles_html += section_header
             if 'sections' in section:
                 articles_html += self.get_articles_from_toc(project, section, level+1)
             articles_html += '</section>'
@@ -261,7 +273,7 @@ class TaConverter(object):
             title = alt_title.strip()
         return title
 
-    def get_article(self, project, link):
+    def get_article(self, project, link, header):
         article_dir = os.path.join(self.ta_dir, project, link)
         question_file = os.path.join(article_dir, 'sub-title.md')
         question = None
@@ -330,7 +342,7 @@ class TaConverter(object):
                     </ul>
                 </div>
                 '''
-        article_html = '<article id="{0}">'.format(link)
+        article_html = '<article id="{0}">{1}'.format(link, header)
         if top_box:
             article_html += '''
             <div class="top-box box">
@@ -346,6 +358,20 @@ class TaConverter(object):
             '''.format(bottom_box)
         article_html += '</article>'
         return article_html
+
+    def download_all_images(self):
+        img_dir = os.path.join(self.html_dir, 'images')
+        os.makedirs(img_dir, exist_ok=True)
+        for img in self.soup.find_all('img'):
+            if 'http' in img['src']:
+                url = img['src']
+                filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url).group(1)
+                img['src'] = 'html/images/{0}'.format(filename)
+                filepath = os.path.join(img_dir, filename)
+                if not os.path.exists(filepath):
+                    with open(filepath, 'wb') as f:
+                        response = requests.get(url)
+                        f.write(response.content)
 
     @staticmethod
     def get_resource_git_url(resource, lang, owner):
