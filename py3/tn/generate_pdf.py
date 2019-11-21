546d9c50462a3351bdeae7a98be4d120bb69d99c
Richard Mahn <rich.mahn@unfoldingword.org>
2019-11-21 02:50:20 -0500

    Fix fetch

--- py3/tn/generate_pdf.py
+++ py3/tn/generate_pdf.py
@@ -364,6 +364,7 @@ class TnConverter(object):
                         if os.path.isdir(repo_dir):
                             break
         g = git.Git(repo_dir)
+        g.fetch()
         g.checkout(tag)
         if tag == DEFAULT_TAG:
             # self.logger.info("not pulling")
