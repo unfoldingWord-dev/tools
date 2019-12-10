8693b815e05da8e041ff80deedc7d5117b752128
Richard Mahn <rich.mahn@unfoldingword.org>
2019-12-10 11:32:22 -0500

    CSS

--- py3/converters/pdf_converter.py
+++ py3/converters/pdf_converter.py
@@ -443,20 +443,16 @@ class PdfConverter:
         parts = re.split(r'\s*…\s*|\s*\.\.\.\s*', phrase)
         processed_text = ''
         to_process_text = text
-        self.logger.info(f'==========\nPHRASE: {phrase}\n')
-        self.logger.info(f'PARTS: {parts}\n')
         for idx, part in enumerate(parts):
-            self.logger.info(f'PART: {part}\n')
+            if not part.strip():
+                continue
             escaped_part = re.escape(part)
-            self.logger.info(f'ESCAPED: {escaped_part}\n')
             if '<span' in text:
                 split_pattern = '(' + re.sub('(\\\\ +)', r'(\\s+|(\\s*</*span[^>]*>\\s*)+)', escaped_part) + ')'
             else:
                 split_pattern = '(' + escaped_part + ')'
             split_pattern += '(?![^<]*>)'  # don't match within HTML tags
-            self.logger.info(f'PATTERN: {split_pattern}\n')
             splits = re.split(split_pattern, to_process_text, 1)
-            self.logger.info(f'SPLITS: {splits}\n\n')
             processed_text += splits[0]
             if len(splits) > 1:
                 highlight_classes = "highlight"
