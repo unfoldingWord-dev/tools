a3f3467bf5a06e3512224c7272f24326c719ad6d
Richard Mahn <rich.mahn@unfoldingword.org>
2019-12-10 11:24:37 -0500

    CSS

--- py3/converters/pdf_converter.py
+++ py3/converters/pdf_converter.py
@@ -450,8 +450,9 @@ class PdfConverter:
             else:
                 split_pattern = '(' + escaped_part + ')'
             split_pattern += '(?![^<]*>)'  # don't match within HTML tags
-            self.logger.info(f'SPLIT PATTERN: {split_pattern}')
             splits = re.split(split_pattern, to_process_text, 1)
+            self.logger.info(f'==========\nPART: {part}\nESCAPED: {escaped_part}\nSPLIT PATTERN: {split_pattern}\n\n')
+            self.logger.info(f'SPLITS: {splits}\n\n')
             processed_text += splits[0]
             if len(splits) > 1:
                 highlight_classes = "highlight"
