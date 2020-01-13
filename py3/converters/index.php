b2fcd0b6a62b7f7399489643331464d2c16c9145
Richard Mahn <rich.mahn@unfoldingword.org>
2020-01-13 10:29:25 -0500

    index.php

--- py3/converters/index.php
+++ py3/converters/index.php
@@ -1,15 +1,30 @@
 <?php
-$files = array();
+$dirs = array();
 $dir = opendir('.'); // open the cwd..also do an err check.
 while(false != ($file = readdir($dir))) {
-        if(($file != ".") and ($file != "..") and ($file != "index.php")) {
-                $files[] = $file; // put in array.
+        if(($file != ".") and ($file != "..") and ($file != "index.php") && is_dir($file)) {
+                $dirs[] = $file; // put in array.
         }
 }
 
-natsort($files); // sort.
+natsort($dirs); // sort.
 
 // print.
-foreach($files as $file) {
+foreach($dirs as $dir) {
+    $files = array();
+    $subdir = opendir($dir); // open the cwd..also do an err check.
+    while(false != ($subfile = readdir($dir))) {
+        if(($subfile != ".") and ($subfile != "..") and is_link()) {
+                $files[] = $subfile; // put in array.
+        }
+    }
+
+    natsort($files); // sort.
+
+    echo '<h2>'.$dir.'</h2>'
+    echo '<p>'
+    foreach($files as $file) {
         echo("<a href='$file'>$file</a> <br />\n");
+    }
+    echo '</p>'
 }
