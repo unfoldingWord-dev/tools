801969fb00811c47692cfff3f1068b9481e782ea
Robert Hunt <Freely.Given.org@gmail.com>
2020-07-29 07:51:46 +1200

    Apr 29 updates

--- tsv/TW2tsv.py
+++ tsv/TW2tsv.py
@@ -21,26 +21,26 @@ LOCAL_NT_SOURCE_FOLDERPATH = LOCAL_SOURCE_BASE_FOLDERPATH.joinpath('el-x-koine_u
 LOCAL_OUTPUT_FOLDERPATH = Path('/mnt/Data/uW_dataRepos/en_tw/')
 
 BBB_NUMBER_DICT = {'GEN':'01','EXO':'02','LEV':'03','NUM':'04','DEU':'05',
-                    'JOS':'06','JDG':'07','RUT':'08','1SA':'09','2SA':'10','1KI':'11',
-                    '2KI':'12','1CH':'13','2CH':'14','EZR':'15',
-                    # 'NEH':'16',
-                    'EST':'17',
-                    'JOB':'18','PSA':'19','PRO':'20','ECC':'21','SNG':'22','ISA':'23',
-                    'JER':'24','LAM':'25','EZK':'26','DAN':'27','HOS':'28','JOL':'29',
-                    'AMO':'30','OBA':'31','JON':'32','MIC':'33','NAM':'34','HAB':'35',
-                    'ZEP':'36','HAG':'37','ZEC':'38','MAL':'39',
-                    'MAT':'41','MRK':'42','LUK':'43','JHN':'44','ACT':'45',
-                    'ROM':'46','1CO':'47','2CO':'48','GAL':'49','EPH':'50','PHP':'51',
-                    'COL':'52','1TH':'53','2TH':'54','1TI':'55','2TI':'56','TIT':'57',
-                    'PHM':'58','HEB':'59','JAS':'60','1PE':'61','2PE':'62','1JN':'63',
-                    '2JN':'64',
-                    '3JN':'65', 'JUD':'66', 'REV':'67'}
+                'JOS':'06','JDG':'07','RUT':'08','1SA':'09','2SA':'10','1KI':'11',
+                '2KI':'12','1CH':'13','2CH':'14','EZR':'15',
+                'NEH':'16',
+                'EST':'17',
+                'JOB':'18','PSA':'19','PRO':'20','ECC':'21','SNG':'22','ISA':'23',
+                'JER':'24','LAM':'25','EZK':'26','DAN':'27','HOS':'28','JOL':'29',
+                'AMO':'30','OBA':'31','JON':'32','MIC':'33','NAM':'34','HAB':'35',
+                'ZEP':'36','HAG':'37','ZEC':'38','MAL':'39',
+                'MAT':'41','MRK':'42','LUK':'43','JHN':'44','ACT':'45',
+                'ROM':'46','1CO':'47','2CO':'48','GAL':'49','EPH':'50','PHP':'51',
+                'COL':'52','1TH':'53','2TH':'54','1TI':'55','2TI':'56','TIT':'57',
+                'PHM':'58','HEB':'59','JAS':'60','1PE':'61','2PE':'62','1JN':'63',
+                '2JN':'64',
+                '3JN':'65', 'JUD':'66', 'REV':'67' }
 
 
 WORD_FIELD_RE = re.compile(r'(\\w .+?\\w\*)')
 SINGLE_WORD_RE = re.compile(r'\\w (.+?)\|')
 SIMPLE_TW_LINK_RE = re.compile(r'x-tw="([:/\*a-z0-9]+?)" \\w\*') # Only occurs inside a \\w field (at end)
-MILESTONE_TW_LINK_RE = re.compile(r'\\k-s \| ?x-tw="([:/\*a-z0-9]+?)" ?\\\*') # Only occurs inside a \\k-s field (at beginning)
+MILESTONE_TW_LINK_RE = re.compile(r'\\k-s \| ?x-tw="([:/\*a-z0-9]+?)" ?\\\*')
 def get_source_lines(BBB:str, nn:str) -> Tuple[str,str,str,str,str,str,str]:
     """
     Generator to read the UGNT book
@@ -74,6 +74,10 @@ def get_source_lines(BBB:str, nn:str) -> Tuple[str,str,str,str,str,str,str]:
                 this_verse_words = []
                 continue
 
+            if line == '\\w עַל|lemma="עַל" strong="H5921a" x-morph="He,R" x-tw="rc://*/tw/dict/bible/other/overseer" \\w*־\\k-s | x-tw="rc://*/tw/dict/bible/names/redsea" \\w יַם|lemma="יָם" strong="H3220" x-morph="He,Ncmsc" \\w*־\\w סֽוּף|lemma="סוּף" strong="H5488" x-morph="He,Ncmsa" \\w*\\k-e\\*׃':
+                print("FIXING BAD NEH 9:9 LINE!!!!")
+                line = '\\w עַל|lemma="עַל" strong="H5921a" x-morph="He,R" x-tw="rc://*/tw/dict/bible/other/overseer" \\w*־\\k-s | x-tw="rc://*/tw/dict/bible/names/redsea"\\* \\w יַם|lemma="יָם" strong="H3220" x-morph="He,Ncmsc" \\w*־\\w סֽוּף|lemma="סוּף" strong="H5488" x-morph="He,Ncmsa" \\w*\\k-e\\*׃'
+
             # Get any words out of line (needed for occurrences)
             this_line_words = []
             word_match = SINGLE_WORD_RE.search(line, 0)
@@ -89,8 +93,12 @@ def get_source_lines(BBB:str, nn:str) -> Tuple[str,str,str,str,str,str,str]:
             # NOTE: Be careful as \\k-s can occur mid-line, e.g., NEH 9:9 !!!
             if '\\k-s' in line:
                 assert not is_in_k
-                is_in_k = True
-            # print(f"{line_number:4}/ {BBB} {C}:{V:<3} {is_in_k} {line}")
+                if line.startswith( '\\k-s'):
+                    is_in_k = True
+                else:
+                    # print(f"NOTE: \\k-s field in the middle of a line: {line}")
+                    is_in_k = 0.5 # !!!!
+            # print(f"{line_number:4}/ {BBB} {C}:{V:<3} is_in_k={is_in_k} line={line}")
 
             # Make sure that the data looks like what we were expecting -- no surprises
             if '\\k' not in line:
@@ -101,21 +109,41 @@ def get_source_lines(BBB:str, nn:str) -> Tuple[str,str,str,str,str,str,str]:
                 assert line.count('\\w ') >= 1
                 assert line.count('\\w*') == line.count('\\w ')
 
-            if is_in_k:
-                if '\\k-s' in line:
-                    assert line.count(' x-tw="') >= 1
-                    milestone_link_match = MILESTONE_TW_LINK_RE.search(line)
-                    if milestone_link_match:
-                        milestone_link = milestone_link_match.group(1)
-                        assert milestone_link.startswith('rc://*/tw/dict/bible/')
-                        remembered_line_number = line_number
-                        milestone_words = []
-                    else:
-                        logging.critical(f"Have a problem with \\k-s on {BBB} {C}:{V} line {line_number:,} in {source_filename}")
-                    # There might still be a \w field on the line, but it's caught below
+            searchW_startIndex = 'BAD' # Just to catch any logic errors below
+            if is_in_k == 0.5: # the \\k-s is MID-LINE
+                assert '\\k-s' in line and not line.startswith('\\k-s')
+                assert line.count(' x-tw="') >= 1
+                milestone_link_match = MILESTONE_TW_LINK_RE.search(line)
+                if milestone_link_match:
+                    milestone_link = milestone_link_match.group(1)
+                    assert milestone_link.startswith('rc://*/tw/dict/bible/')
+                    remembered_line_number = line_number
+                    milestone_words = []
+                    searchW_startIndex = milestone_link_match.end()
+                else:
+                    logging.critical(f"Have a problem with \\k-s on {BBB} {C}:{V} line {line_number:,} in {source_filename}")
                 if '\\k-e' in line:
-                    assert '\\w' not in line
-                    assert 'x-tw' not in line
+                    ke_index = line.index('\\k-e')
+
+                    assert len(this_line_words) >= 1
+                    word_field_match = WORD_FIELD_RE.search(line, searchW_startIndex)
+                    while word_field_match:
+                        word_field = word_field_match.group(1)
+                        word_match = SINGLE_WORD_RE.search(word_field)
+                        assert word_match
+                        word = word_match.group(1)
+                        assert word_match.start() < ke_index
+                        occurrence = this_verse_words.count(word)
+                        if is_in_k: milestone_words.append(word)
+                        simple_link_match = SIMPLE_TW_LINK_RE.search(word_field)
+                        if simple_link_match:
+                            word_link = simple_link_match.group(1)
+                            assert word_link.startswith('rc://*/tw/dict/bible/')
+                            # print("here2", C,V, word, occurrence, this_verse_words, word_link)
+                            yield line_number, BBB, C, V, word, occurrence, word_link
+                        word_field_match = WORD_FIELD_RE.search(line, word_field_match.end())
+                    searchW_startIndex = 'BADBAD' # Just to catch any logic errors
+
                     is_in_k = False
                     assert milestone_words
                     milestone_words = ' '.join(milestone_words)
@@ -123,26 +151,42 @@ def get_source_lines(BBB:str, nn:str) -> Tuple[str,str,str,str,str,str,str]:
                     yield remembered_line_number, BBB, C, V, milestone_words, occurrence, milestone_link
                     del remembered_line_number, milestone_words, milestone_link # Don't let them persist -- just so we catch any logic errors
                     continue
-            #     if '\\w ' in line:
-            #         assert len(this_line_words) >= 1
-            #         word_field_match = WORD_FIELD_RE.search(line, 0)
-            #         while word_field_match:
-            #             word_field = word_field_match.group(1)
-            #             word_match = SINGLE_WORD_RE.search(word_field)
-            #             assert word_match
-            #             word = word_match.group(1)
-            #             occurrence = this_verse_words.count(word)
-            #             simple_link_match = SIMPLE_TW_LINK_RE.search(word_field)
-            #             if simple_link_match:
-            #                 word_link = simple_link_match.group(1)
-            #                 assert word_link.startswith('rc://*/tw/dict/bible/')
-            #                 # print("here1", C,V, word, occurrence, this_verse_words, word_link)
-            #                 yield line_number, BBB, C, V, word, occurrence, word_link
-            #             word_field_match = WORD_FIELD_RE.search(line, word_field_match.end())
+                else: # k-s is fully open now
+                    is_in_k = True
+                # There might still be a \w field on the line, but it's caught below
+            elif is_in_k:
+                if '\\k-s' in line:
+                    assert line.startswith('\\k-s ')
+                    assert line.count(' x-tw="') >= 1
+                    milestone_link_match = MILESTONE_TW_LINK_RE.search(line)
+                    if milestone_link_match:
+                        milestone_link = milestone_link_match.group(1)
+                        assert milestone_link.startswith('rc://*/tw/dict/bible/')
+                        remembered_line_number = line_number
+                        milestone_words = []
+                        searchW_startIndex = milestone_link_match.end()
+                    else:
+                        logging.critical(f"Have a problem with \\k-s on {BBB} {C}:{V} line {line_number:,} in {source_filename}")
+                    # There might still be a \w field on the line, but it's caught below
+                    assert '\\k-e' not in line
+                elif '\\k-e' in line:
+                    assert line.startswith('\\k-e\\*')
+                    assert '\\w' not in line  #  } Don't really care about
+                    assert 'x-tw' not in line #  }  any left-over Hebrew chars in line
+                    is_in_k = False
+                    assert milestone_words
+                    milestone_words = ' '.join(milestone_words)
+                    # print("here0", C,V, milestone_words, occurrence, milestone_link)
+                    yield remembered_line_number, BBB, C, V, milestone_words, occurrence, milestone_link
+                    del remembered_line_number, milestone_words, milestone_link # Don't let them persist -- just so we catch any logic errors
+                    continue
+                else: # the simpler single line case -- usually one, sometimes two \\w fields on a line
+                    searchW_startIndex = 0 # Look for words right from the beginning of the line
+            else: # the simpler single line case -- usually one, sometimes two \\w fields on a line
+                searchW_startIndex = 0 # Look for words right from the beginning of the line
 
-            # else: # the simpler single line case -- usually one, sometimes two \\w fields on a line
             assert len(this_line_words) >= 1
-            word_field_match = WORD_FIELD_RE.search(line, 0)
+            word_field_match = WORD_FIELD_RE.search(line, searchW_startIndex)
             while word_field_match:
                 word_field = word_field_match.group(1)
                 word_match = SINGLE_WORD_RE.search(word_field)
@@ -157,6 +201,7 @@ def get_source_lines(BBB:str, nn:str) -> Tuple[str,str,str,str,str,str,str]:
                     # print("here2", C,V, word, occurrence, this_verse_words, word_link)
                     yield line_number, BBB, C, V, word, occurrence, word_link
                 word_field_match = WORD_FIELD_RE.search(line, word_field_match.end())
+            searchW_startIndex = 'BADBADBAD' # Just to catch any logic errors above
 # end of get_source_lines function
 
 
@@ -168,7 +213,7 @@ def make_TSV_file(BBB:str, nn:str) -> Tuple[int,int]:
     output_filepath = LOCAL_OUTPUT_FOLDERPATH.joinpath(f'en_twl_{BBB_NUMBER_DICT[BBB]}-{BBB}.tsv')
     num_simple_links = num_complex_links = 0
     with open(output_filepath, 'wt') as output_TSV_file:
-        output_TSV_file.write('Book	Chapter	Verse ID	SupportReference	OrigQuote	Occurrence	GLQuote	OccurrenceNote\n')
+        output_TSV_file.write('Book\tChapter\tVerse\tID\tSupportReference\tOrigQuote\tOccurrence\tGLQuote\tOccurrenceNote\n')
         previous_ids:List[str] = ['']
         for j, (line_number,BBB,C,V,word,occurrence,link) in enumerate(get_source_lines(BBB, nn), start=1):
             # print(f"{j:3}/ Line {line_number:<5} {BBB} {C:>3}:{V:<3} '{word}' {occurrence} {link}")
@@ -177,7 +222,7 @@ def make_TSV_file(BBB:str, nn:str) -> Tuple[int,int]:
                 generated_id = random.choice('abcdefghijklmnopqrstuvwxyz') + random.choice('abcdefghijklmnopqrstuvwxyz0123456789') + random.choice('abcdefghijklmnopqrstuvwxyz0123456789') + random.choice('abcdefghijklmnopqrstuvwxyz0123456789')
             previous_ids.append(generated_id)
 
-            output_TSV_file.write(f'{BBB}	{C}	{V}	{generated_id}	{link}	{word}	{occurrence}	GLQuote	OccurrenceNote\n')
+            output_TSV_file.write(f'{BBB}\t{C}\t{V}\t{generated_id}\t{link}\t{word}\t{occurrence}\t(GLQuote)\t(OccurrenceNote)\n')
             if ' ' in word: num_complex_links += 1
             else: num_simple_links += 1
     print(f"      {j:,} links written ({num_simple_links:,} simple links and {num_complex_links:,} complex links)")
