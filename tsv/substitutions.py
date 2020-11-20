6be24e4d2c02fd5787b6e6f8694d8c8e59829349
Larry Versaw <lversaw@gmail.com>
2020-11-20 08:33:39 -0700

    used by tsv_cleanup.tsv

---
+++ tsv/substitutions.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# substitutions module, used by tsv_cleanup.py.
+# An ordered list of tuples to be used for string substitutions.
+
+subs = [
+ 	("\\[\\[", "[["),
+ 	("\\]\\]", "]]"),
+ 	("# <br>", "# "),
+ 	("rc://en_ta/", "rc://kn/ta/"),
+ 	("rc://en/", "rc://kn/")
+]
+
+#	("rc://en/", "rc://hi/"),
+# 	("//ro/t", "//*/t") # specific to Oriya tN
+# 	("translate/writing-connectingwords", "translate/grammar-connect-words-phrases")
+#   ("translate/grammar-connect-words-phrases", "translate/writing-connectingwords")
+# The writing-connectingwords --> grammar-connect-words-phrases may well be incorrect for GLs that have their own tA resource.
