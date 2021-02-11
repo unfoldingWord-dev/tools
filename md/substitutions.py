dccd1afba5e4df81efe9dd3060661c435af37cef
Larry Versaw <lversaw@gmail.com>
2021-02-11 16:58:08 -0700

    markdown cleanup refinements

--- md/substitutions.py
+++ md/substitutions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# substitutions module, used by translate.py.
+# substitutions module, used by md_cleanup.py.
 # An ordered list of tuples to be used for string substitutions.
 # The first element is a regular express, the second is a string to replace it with
 #
@@ -7,9 +7,12 @@
 subs = [
 	("http://ufw.io/[[rc://", "[[rc://"),
 	("&nbsp;", " "),
+	("<o:p>", ""),
+	("</o:p>", ""),
 	("rc://en/", "rc://*/"),
-	("rc://te/", "rc://*/"),
-	("rc://te /", "rc://*/"),
+	("rc://*/obe/", "rc://*/tw/bible/"),
 	(") [", "), ["),
-	(" \( ", " (")
+	(" \( ", " ("),
+	("/ )", "/)"),
+	("..md", ".md")
 ]
