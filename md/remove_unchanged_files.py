114aeec86f4b5957ea294d61a6b9e3d5dcc1c094
Larry Versaw <lversaw@gmail.com>
2022-03-18 17:28:55 -0600

    refinements

--- md/remove_unchanged_files.py
+++ md/remove_unchanged_files.py
@@ -8,17 +8,13 @@
 # AFTER RUNNING THIS SCRIPT
 # Remove the older folder and replace it with a complete clone of the updated repo
 
-updated_dir = r'C:\DCS\Russian\TW.Jun-21'    # new source folder, from which files will be removed
-older_dir = r'C:\DCS\Russian\TW.May-21'  # make both of these paths as low level as possible to avoid removing too many files
-maxRemoved = 11111
+updated_dir = r'C:\DCS\Hindi\TN'    # new source folder, from which files will be removed
+older_dir = r'C:\DCS\Hindi\TN.Dec-21'  # make both of these paths as low level as possible to avoid removing too many files
 nRemoved = 0
 nLeft = 0
 
-#import re       # regular expression module
-#import io
 import os
 from filecmp import cmp
-#import string
 import sys
 
 # Compares the files and deletes the new file if they are identical
@@ -28,16 +24,14 @@ def processFile(newfile, oldfile):
     if cmp(newfile, oldfile, shallow=False):
         os.remove(newfile)
         print("Removed " + newfile)
-        nRemoved += 1  
+        nRemoved += 1
     else:
-        nLeft += 1  
+        nLeft += 1
 
 # Recursive routine to remove files from updated folder that are identical in older folder
 def processDir(updated, older):
     global nRemoved
     global nLeft
-    if nRemoved >= maxRemoved: 
-        return
     for entry in os.listdir(updated):
         if entry[0] == '.':
             continue
@@ -45,14 +39,14 @@ def processDir(updated, older):
         oldpath = os.path.join(older, entry)
         if os.path.isdir(newpath) and os.path.isdir(oldpath):
             processDir(newpath, oldpath)
-        elif os.path.isfile(newpath) and os.path.isfile(oldpath):
+        elif os.path.isfile(oldpath):
             processFile(newpath, oldpath)
-        if nRemoved >= maxRemoved:
-            break
+        else:
+            nLeft += 1
     if os.listdir(updated) == []:
         os.rmdir(updated)          # remove entire folder if empty
         print("Removed folder: " + updated)
-        nRemoved += 1  
+        nRemoved += 1
     else:
         nLeft += 1
 
@@ -61,9 +55,9 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] != 'hard-coded-path':
         source_dir = sys.argv[1]
 
-    if updated_dir and updated_dir != older_dir and os.path.isdir(updated_dir):
+    if updated_dir != older_dir and os.path.isdir(updated_dir) and os.path.isdir(older_dir):
         processDir(updated_dir, older_dir)
         print("\nDone. Removed " + str(nRemoved) + " unchanged files and folders. " + str(nLeft) + " were changed and remain.")
-        print("When done, replace the older directory with a full clone of the updated one.")
+        print("To facilitate future updates, be sure to keep a full clone of the most recent repo.")
     else:
         sys.stderr.write("Usage: python remove_unchanged_files.py <updated_folder>\n  Use . for current folder. Set globals before running this script.\n")
